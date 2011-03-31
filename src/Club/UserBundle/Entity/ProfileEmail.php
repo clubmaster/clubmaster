8c7bb95b59d30d6c99c63d73e2daf53779aa5bbf
Michael Holm <hollo@hollo.dk>
2011-03-31 23:37:37 +0200

    Added entity files

---
+++ src/Club/UserBundle/Entity/ProfileEmail.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Club\UserBundle\Entity;
+
+/**
+ * Club\UserBundle\Entity\ProfileEmail
+ */
+class ProfileEmail
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
