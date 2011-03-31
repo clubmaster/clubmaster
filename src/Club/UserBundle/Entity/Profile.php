0d04c3e37f850ffcd179bbd8a08138f528b23d66
Michael Holm <hollo@hollo.dk>
2011-04-01 00:12:30 +0200

    Added some columns to our model

--- src/Club/UserBundle/Entity/Profile.php
+++ src/Club/UserBundle/Entity/Profile.php
@@ -22,4 +22,104 @@ class Profile
     {
         return $this->id;
     }
+    /**
+     * @var integer $user_id
+     */
+    private $user_id;
+
+    /**
+     * @var string $first_name
+     */
+    private $first_name;
+
+    /**
+     * @var string $last_name
+     */
+    private $last_name;
+
+    /**
+     * @var Club\UserBundle\Entity\User
+     */
+    private $user;
+
+
+    /**
+     * Set user_id
+     *
+     * @param integer $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->user_id = $userId;
+    }
+
+    /**
+     * Get user_id
+     *
+     * @return integer $userId
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set first_name
+     *
+     * @param string $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->first_name = $firstName;
+    }
+
+    /**
+     * Get first_name
+     *
+     * @return string $firstName
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * Set last_name
+     *
+     * @param string $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->last_name = $lastName;
+    }
+
+    /**
+     * Get last_name
+     *
+     * @return string $lastName
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * Set user
+     *
+     * @param Club\UserBundle\Entity\User $user
+     */
+    public function setUser(\Club\UserBundle\Entity\User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get user
+     *
+     * @return Club\UserBundle\Entity\User $user
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
 }
--- src/Club/UserBundle/Entity/~User.php
+++ src/Club/UserBundle/Entity/~User.php
@@ -22,4 +22,129 @@ class User
     {
         return $this->id;
     }
+    /**
+     * @var integer $profile_id
+     */
+    private $profile_id;
+
+    /**
+     * @var string $username
+     */
+    private $username;
+
+    /**
+     * @var email $email_address
+     */
+    private $email_address;
+
+    /**
+     * @var string $password
+     */
+    private $password;
+
+    /**
+     * @var Club\UserBundle\Entity\Profile
+     */
+    private $profile;
+
+
+    /**
+     * Set profile_id
+     *
+     * @param integer $profileId
+     */
+    public function setProfileId($profileId)
+    {
+        $this->profile_id = $profileId;
+    }
+
+    /**
+     * Get profile_id
+     *
+     * @return integer $profileId
+     */
+    public function getProfileId()
+    {
+        return $this->profile_id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string $username
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email_address
+     *
+     * @param email $emailAddress
+     */
+    public function setEmailAddress(\email $emailAddress)
+    {
+        $this->email_address = $emailAddress;
+    }
+
+    /**
+     * Get email_address
+     *
+     * @return email $emailAddress
+     */
+    public function getEmailAddress()
+    {
+        return $this->email_address;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string $password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set profile
+     *
+     * @param Club\UserBundle\Entity\Profile $profile
+     */
+    public function setProfile(\Club\UserBundle\Entity\Profile $profile)
+    {
+        $this->profile = $profile;
+    }
+
+    /**
+     * Get profile
+     *
+     * @return Club\UserBundle\Entity\Profile $profile
+     */
+    public function getProfile()
+    {
+        return $this->profile;
+    }
 }
