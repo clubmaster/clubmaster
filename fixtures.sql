ed99a5aceee3b3a444f2c34c04c1ee7e7e3e8b7d
Michael Holm <hollo@hollo.dk>
2011-04-14 17:20:50 +0200

    Tried to find some requirements for the project.

--- fixtures.sql
+++ fixtures.sql
@@ -33,7 +33,8 @@ INSERT INTO club_shop_payment_method (payment_method_name) VALUES
 INSERT INTO club_shop_product (product_name,description,price,tax_id) VALUES
   ('1 md.','1 md.',100,2),
   ('2 md.','2 md.',150,2),
-  ('10 bolde','10 bolde',50,2);
+  ('Season subscription',200,2),
+  ('10 balls','10 balls',50,2);
 
 INSERT INTO product_category (product_id,category_id) VALUES
   (1,1),
@@ -45,3 +46,14 @@ INSERT INTO club_shop_currency (currency_name,code,symbol_left,symbol_right,deci
 
 INSERT INTO club_language (name,code,locale,charset,date_format_short,date_format_long,time_format,text_direction,numeric_separator_decimal,numeric_separator_thousands,currency_id) VALUES
   ('Danish','da_DK','da_DK.UTF-8,da_DK,danish','utf-8','%d/%m/%Y','%A %d %B, %Y','%H:%i:%s','ltr',',','.',1);
+
+INSERT INTO club_shop_product_variant_group (title,module) VALUES
+  ('Subscription month','Subscription'),
+  ('Ticket','TicketCoupon');
+
+INSERT INTO club_shop_product_variant_value (product_variant_group_id,title) VALUES
+  (1,1),
+  (1,'TicketCoupon');
+
+INSERT INTO club_shop_product_variant (product_id,product_variant_value_id) VALUES
+  (1,1
