5005558126c2d2d67cb6759818196f12d9788bac
Michael Holm <hollo@hollo.dk>
2011-04-19 17:38:04 +0200

    Added category list view.

--- src/Club/ShopBundle/Controller/ShopController.php
+++ src/Club/ShopBundle/Controller/ShopController.php
@@ -3,24 +3,44 @@
 namespace Club\ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ShopController extends Controller
 {
-    /**
-     * @extra:Route("/shop/category", name="shop_category")
-     * @extra:Template()
-     */
-    public function categoryAction()
-    {
-      return array();
-    }
+  /**
+   * @extra:Route("/shop/category", name="shop_category")
+   * @extra:Template()
+   */
+  public function categoryAction()
+  {
+    $em = $this->get('doctrine.orm.entity_manager');
 
-    /**
-     * @extra:Route("/shop/order", name="shop_order")
-     * @extra:Template()
-     */
-    public function orderAction()
-    {
-      return array();
-    }
+    $categories = $em->getRepository('Club\ShopBundle\Entity\Category')->findAll();
+
+    return array(
+      'categories' => $categories
+    );
+  }
+
+  /**
+   * @extra:Route("/shop/category/delete/{id}", name="shop_category_delete")
+   */
+  public function categoryDeleteAction($id)
+  {
+    $em = $this->get('doctrine.orm.entity_manager');
+    $category = $em->find('Club\ShopBundle\Entity\Category',$id);
+
+    $em->remove($category);
+    $em->flush();
+
+    return new RedirectResponse($this->generateUrl('shop_category'));
+  }
+
+  /**
+   * @extra:Route("/shop/category/edit/{id}", name="shop_category_edit")
+   */
+  public function categoryEditAction($id)
+  {
+    return array();
+  }
 }
