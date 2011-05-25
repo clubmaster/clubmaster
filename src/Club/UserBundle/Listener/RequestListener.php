1023b0a50281683452bb69c0d25929ba78e62345
Michael Holm <hollo@hollo.dk>
2011-05-25 16:36:35 +0200

    Added entity manger to user listerner.

--- src/Club/UserBundle/Listener/RequestListener.php
+++ src/Club/UserBundle/Listener/RequestListener.php
@@ -7,6 +7,13 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class RequestListener
 {
+  protected $em;
+
+  public function __construct($em)
+  {
+    $this->em = $em;
+  }
+
   public function onCoreRequest(GetResponseEvent $event)
   {
   }
