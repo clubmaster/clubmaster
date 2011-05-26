cf4b394db0e4f710d305bf662cd2ad8e306001e8
Michael Holm <hollo@hollo.dk>
2011-05-26 14:51:58 +0200

    Added basic CRUD for orders.

--- src/Club/ShopBundle/Controller/AdminOrderController.php
+++ src/Club/ShopBundle/Controller/AdminOrderController.php
@@ -15,5 +15,59 @@ class AdminOrderController extends Controller
    */
   public function indexAction()
   {
+    $em = $this->get('doctrine.orm.entity_manager');
+    $orders = $em->getRepository('\Club\ShopBundle\Entity\Order')->findAll();
+
+    return array(
+      'orders' => $orders
+    );
+  }
+
+  /**
+   * @Route("/shop/order/edit/{id}", name="admin_shop_order_edit")
+   * @Template()
+   */
+  public function editAction($id)
+  {
+    $em = $this->get('doctrine.orm.entity_manager');
+    $order = $em->find('Club\ShopBundle\Entity\Order',$id);
+
+    $res = $this->process($order);
+
+    if ($res instanceOf RedirectResponse)
+      return $res;
+
+    return array(
+      'order' => $order,
+      'page' => array('header' => 'Order'),
+      'form' => $res->createView()
+    );
+  }
+
+  /**
+   * @Route("/shop/order/batch", name="admin_shop_order_batch")
+   */
+  public function batchAction()
+  {
+  }
+
+  protected function process($order)
+  {
+    $form = $this->get('form.factory')->create(new \Club\ShopBundle\Form\Order(), $order);
+
+    if ($this->get('request')->getMethod() == 'POST') {
+      $form->bindRequest($this->get('request'));
+      if ($form->isValid()) {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $em->persist($order);
+        $em->flush();
+
+        $this->get('session')->setFlash('notice','Your changes were saved!');
+
+        return new RedirectResponse($this->generateUrl('admin_shop_order'));
+      }
+    }
+
+    return $form;
   }
 }
