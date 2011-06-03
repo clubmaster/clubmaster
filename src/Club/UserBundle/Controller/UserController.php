361cc632a7334f5da3f8411ee0a63107b5fb1ae3
Michael Holm <hollo@hollo.dk>
2011-06-03 11:26:33 +0200

    Added user controller.

--- src/Club/UserBundle/Controller/UserController.php
+++ src/Club/UserBundle/Controller/UserController.php
@@ -17,9 +17,23 @@ class UserController extends Controller
   public function indexAction()
   {
     $user = $this->get('security.context')->getToken()->getUser();
+    $form = $this->get('form.factory')->create(new \Club\UserBundle\Form\User(), $user);
+
+    if ($this->get('request')->getMethod() == 'POST') {
+      $form->bindRequest($this->get('request'));
+
+      if ($form->isValid()) {
+        $em = $this->get('doctrine.orm.entity_manager');
+        $em->persist($user);
+        $em->flush();
+
+        return new RedirectResponse($this->generateUrl('user'));
+      }
+    }
 
     return array(
-      'user' => $user
+      'user' => $user,
+      'form' => $form->createView()
     );
   }
 }
