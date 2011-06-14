54823f09101906390b4f228b1775601b9f2f244d
Michael Holm <hollo@hollo.dk>
2011-06-14 14:31:38 +0200

    Added overview of login attempts.

--- src/Club/LogBundle/Controller/LogController.php
+++ src/Club/LogBundle/Controller/LogController.php
@@ -27,58 +27,12 @@ class LogController extends Controller
   {
     $em = $this->get('doctrine')->getEntityManager();
 
-    $logs = $em->getRepository('\Club\LogBundle\Entity\Log')->findAll();
+    $logs = $em->getRepository('ClubLogBundle:Log')->findAll();
+    $login_attempts = $em->getRepository('ClubUserBundle:LoginAttempt')->findAll();
 
     return array(
-      'logs' => $logs
+      'logs' => $logs,
+      'login_attempts' => $login_attempts
     );
-  }
-
-  /**
-   * @Route("/log/disable/{id}", name="admin_log_disable")
-   */
-  public function disableAction($id)
-  {
-    $em = $this->get('doctrine')->getEntityManager();
-
-    $log = $em->find('\Club\LogBundle\Entity\Log',$id);
-    $log->setEnabled(0);
-
-    $em->persist($log);
-    $em->flush();
-
-    return new RedirectResponse($this->generateUrl('admin_log'));
-  }
-
-  /**
-   * @Route("/log/enable/{id}", name="admin_log_enable")
-   */
-  public function enableAction($id)
-  {
-    $em = $this->get('doctrine')->getEntityManager();
-
-    $log = $em->find('\Club\LogBundle\Entity\Log',$id);
-    $log->setEnabled(1);
-
-    $em->persist($log);
-    $em->flush();
-
-    return new RedirectResponse($this->generateUrl('admin_log'));
-  }
-
-  /**
-   * @Route("/log/run/{id}", name="admin_log_run")
-   */
-  public function runAction($id)
-  {
-    $em = $this->get('doctrine')->getEntityManager();
-
-    $log = $em->find('\Club\LogBundle\Entity\Log',$id);
-    $log->setNextRunAt(new \DateTime());
-
-    $em->persist($log);
-    $em->flush();
-
-    return new RedirectResponse($this->generateUrl('admin_log'));
   }
 }
