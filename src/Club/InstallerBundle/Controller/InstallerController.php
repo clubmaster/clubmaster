fbec983b592d3fa36b53620c797788536821e241
Michael Holm <hollo@hollo.dk>
2011-06-23 16:15:56 +0200

    Added init controllers to bundle.

---
+++ src/Club/InstallerBundle/Controller/InstallerController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Club\InstallerBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class InstallerController extends Controller
+{
+  /**
+   * @Route("/installer")
+   * @Template()
+   */
+  public function indexAction()
+  {
+    return array();
+  }
+
+  /**
+   * @Route("/installer/check")
+   * @Template()
+   */
+  public function checkAction()
+  {
+    return array();
+  }
+
+  /**
+   * @Route("/installer/database")
+   * @Template()
+   */
+  public function databaseAction()
+  {
+    return array();
+  }
+
+  /**
+   * @Route("/installer/administrator")
+   * @Template()
+   */
+  public function administratorAction()
+  {
+    return array();
+  }
+
+  /**
+   * @Route("/installer/settings")
+   * @Template()
+   */
+  public function settingsAction()
+  {
+    return array();
+  }
+
+  /**
+   * @Route("/installer/confirm")
+   * @Template()
+   */
+  public function confirmAction()
+  {
+    return array();
+  }
+}
