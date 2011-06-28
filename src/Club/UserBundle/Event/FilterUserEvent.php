cb91f6d22c47cce2cae813671571f2a2dff1e742
Michael Holm <hollo@hollo.dk>
2011-06-28 21:22:53 +0200

    Style register new user.

--- src/Club/UserBundle/Event/FilterUserEvent.php
+++ src/Club/UserBundle/Event/FilterUserEvent.php
@@ -8,19 +8,13 @@ class FilterUserEvent extends Event
 {
   protected $user;
 
-  public function __construct(\Club\UserBundle\Entity\User $user, \Club\UserBundle\Entity\ForgotPassword $forgot_password)
+  public function __construct(\Club\UserBundle\Entity\User $user)
   {
     $this->user = $user;
-    $this->forgot_password = $forgot_password;
   }
 
   public function getUser()
   {
     return $this->user;
-  }
-
-  public function getForgotPassword()
-  {
-    return $this->forgot_password;
   }
 }
