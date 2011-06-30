23a15e8f02056d0f55e15580d73b30319c3e3b03
Michael Holm <hollo@hollo.dk>
2011-06-30 20:18:40 +0200

    Improved installer.

--- src/Club/install.sh
+++ src/Club/install.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
 
+chmod -R 777 app/cache app/logs
 echo
 php app/check.php
 
 echo
-echo "  ####################################################"
-echo "  #                                                  #"
-echo "  # A System check has just been processed, please   #"
-echo "  # use a few seconds that you meet all the          #"
-echo "  # requirements.                                    #"
-echo "  #                                                  #"
-echo "  ####################################################"
-echo
-echo "  Press a key to continue..."
-read
-
-echo
-echo "  ####################################################"
-echo "  #                                                  #"
-echo "  # INFO: If you already have created the database,  #"
-echo "  # you can add the parameter \"drop\" to the        #"
-echo "  # script. The database will be dropped.            #"
-echo "  #                                                  #"
-echo "  ####################################################"
+echo "****************************************************"
+echo "*                                                  *"
+echo "* INFO: If you already have created the database,  *"
+echo "* you can add the parameter \"drop\" to the        *"
+echo "* script. The database will be dropped.            *"
+echo "*                                                  *"
+echo "****************************************************"
 echo
 echo "  Press a key to continue..."
 read
@@ -36,3 +25,14 @@ php app/console doctrine:database:create
 php app/console doctrine:migrations:migrate --no-interaction
 php app/console doctrine:fixtures:load
 chmod -R 777 app/cache app/logs
+
+echo
+echo
+echo "*********************************************************"
+echo "*                                                       *"
+echo "* ATTENTION:                                            *"
+echo "*   Now you can progress with the online configuration. *"
+echo "*   Open your browser and locate the file web/check.php *"
+echo "*                                                       *"
+echo "*********************************************************"
+echo
