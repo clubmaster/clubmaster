5a5b1f0c1e4ddb8b53f3fe0bc32adf6ede8cb25b
Michael Holm <hollo@hollo.dk>
2011-07-05 14:32:02 +0200

    Reall all tasks on upgrade.

--- app/DoctrineFixtures/20110705133022/LoadTasksBeta3Data.php
+++ app/DoctrineFixtures/20110705133022/LoadTasksBeta3Data.php
@@ -8,6 +8,53 @@ class LoadTasksBeta3Data implements FixtureInterface
 {
   public function load($manager)
   {
+    $tasks = $manager->getRepository('ClubTaskBundle:Task')->findAll();
+
+    foreach ($tasks as $task) {
+      $manager->remove($task);
+    }
+    $manager->flush();
+
+    $task = new \Club\TaskBundle\Entity\Task();
+    $task->setTaskName('Update dynamic groups');
+    $task->setEnabled(1);
+    $task->setLocked(0);
+    $task->setNextRunAt(new \DateTime());
+    $task->setTaskInterval('+1 hour');
+    $task->setEvent('\Club\TaskBundle\Event\Events');
+    $task->setMethod('onGroupTask');
+    $manager->persist($task);
+
+    $task = new \Club\TaskBundle\Entity\Task();
+    $task->setTaskName('Cleanup logs');
+    $task->setEnabled(1);
+    $task->setLocked(0);
+    $task->setNextRunAt(new \DateTime());
+    $task->setTaskInterval('+1 hour');
+    $task->setEvent('\Club\TaskBundle\Event\Events');
+    $task->setMethod('onLogTask');
+    $manager->persist($task);
+
+    $task = new \Club\TaskBundle\Entity\Task();
+    $task->setTaskName('Renewal memberships');
+    $task->setEnabled(1);
+    $task->setLocked(0);
+    $task->setNextRunAt(new \DateTime());
+    $task->setTaskInterval('+1 hour');
+    $task->setEvent('\Club\TaskBundle\Event\Events');
+    $task->setMethod('onAutoRenewalTask');
+    $manager->persist($task);
+
+    $task = new \Club\TaskBundle\Entity\Task();
+    $task->setTaskName('Cleanup login logs');
+    $task->setEnabled(1);
+    $task->setLocked(0);
+    $task->setNextRunAt(new \DateTime());
+    $task->setTaskInterval('+1 hour');
+    $task->setEvent('\Club\TaskBundle\Event\Events');
+    $task->setMethod('onLoginAttemptTask');
+    $manager->persist($task);
+
     $task = new \Club\TaskBundle\Entity\Task();
     $task->setTaskName('Cleanup ban logs');
     $task->setEnabled(1);
