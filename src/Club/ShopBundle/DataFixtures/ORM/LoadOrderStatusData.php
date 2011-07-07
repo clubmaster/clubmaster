3dc1e13ce3c5b895d140a996601550d4fe06f285
Michael Holm <hollo@hollo.dk>
2011-07-07 17:08:46 +0200

    Make sure we always see not closed orders.

--- src/Club/ShopBundle/DataFixtures/ORM/LoadOrderStatusData.php
+++ src/Club/ShopBundle/DataFixtures/ORM/LoadOrderStatusData.php
@@ -12,35 +12,35 @@ class LoadOrderStatusData implements FixtureInterface
     $status->setStatusName('Pending');
     $status->setIsAccepted(0);
     $status->setIsCancelled(0);
-    $status->setPriority(0);
+    $status->setPriority(1);
     $manager->persist($status);
 
     $status = new \Club\ShopBundle\Entity\OrderStatus();
     $status->setStatusName('Processing');
     $status->setIsAccepted(0);
     $status->setIsCancelled(0);
-    $status->setPriority(0);
+    $status->setPriority(2);
     $manager->persist($status);
 
     $status = new \Club\ShopBundle\Entity\OrderStatus();
     $status->setStatusName('Preparing');
     $status->setIsAccepted(0);
     $status->setIsCancelled(0);
-    $status->setPriority(0);
+    $status->setPriority(3);
     $manager->persist($status);
 
     $status = new \Club\ShopBundle\Entity\OrderStatus();
     $status->setStatusName('Delivered');
     $status->setIsAccepted(1);
     $status->setIsCancelled(0);
-    $status->setPriority(0);
+    $status->setPriority(4);
     $manager->persist($status);
 
     $status = new \Club\ShopBundle\Entity\OrderStatus();
     $status->setStatusName('Cancelled');
     $status->setIsAccepted(0);
     $status->setIsCancelled(1);
-    $status->setPriority(0);
+    $status->setPriority(5);
     $manager->persist($status);
 
     $manager->flush();
