1b07000bec72b8cf1596814cf00abf175bb59bb8
Michael Holm <hollo@hollo.dk>
2011-07-07 16:16:43 +0200

    Add new account.

--- src/Club/AccountBundle/Controller/AdminAccountController.php
+++ src/Club/AccountBundle/Controller/AdminAccountController.php
@@ -23,4 +23,33 @@ class AdminAccountController extends Controller
       'accounts' => $accounts
     );
   }
+
+  /**
+   * @Route("/account/account/new")
+   * @Template
+   */
+  public function newAction()
+  {
+    $account = new \Club\AccountBundle\Entity\Account();
+    $form = $this->createForm(new \Club\AccountBundle\Form\Account(), $account);
+
+    if ($this->getRequest()->getMethod() == 'POST') {
+      $form->bindRequest($this->getRequest());
+
+      if ($form->isValid()) {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $em->persist($account);
+        $em->flush();
+
+        $this->get('session')->setFlash('notice','Your changes has been saved.');
+        return $this->redirect($this->generateUrl('club_account_adminaccount_index', array('id' => $account->getId())));
+      }
+    }
+
+    return array(
+      'form' => $form->createView()
+    );
+  }
+
 }
