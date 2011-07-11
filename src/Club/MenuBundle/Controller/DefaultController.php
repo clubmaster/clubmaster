41814c38536bf90cbbb3a65f88bf6d0e3149e400
Michael Holm <hollo@hollo.dk>
2011-07-11 14:53:09 +0200

    Moved top menu to menubundle.

--- src/Club/MenuBundle/Controller/DefaultController.php
+++ src/Club/MenuBundle/Controller/DefaultController.php
@@ -7,9 +7,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    
-    public function indexAction($name)
-    {
-        return $this->render('ClubMenuBundle:Default:index.html.twig', array('name' => $name));
-    }
+  public function topMenuAction()
+  {
+    $menu = array();
+
+    $menu[] = array(
+      'name' => 'User',
+      'route' => $this->generateUrl('user')
+    );
+    $menu[] = array(
+      'name' => 'Shop',
+      'route' => $this->generateUrl('shop')
+    );
+    $menu[] = array(
+      'name' => 'Order',
+      'route' => $this->generateUrl('shop_order')
+    );
+    $menu[] = array(
+      'name' => 'Subscription',
+      'route' => $this->generateUrl('shop_subscription')
+    );
+    $menu[] = array(
+      'name' => 'Event',
+      'route' => $this->generateUrl('event_event')
+    );
+
+    return $this->render('ClubMenuBundle:Default:topMenu.html.twig', array(
+      'menu' => $menu
+    ));
+  }
 }
