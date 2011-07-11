266c7419d850f62e8d7100038aa733f9d46ee7ee
Michael Holm <hollo@hollo.dk>
2011-07-11 15:12:57 +0200

    Moved left menu to menu bundle.

--- src/Club/MenuBundle/Controller/DefaultController.php
+++ src/Club/MenuBundle/Controller/DefaultController.php
@@ -36,4 +36,80 @@ class DefaultController extends Controller
       'menu' => $menu
     ));
   }
+
+  public function leftMenuAction()
+  {
+    $menu = array(
+      array(
+        'name' => 'User',
+        'route' => $this->generateUrl('admin_user'),
+        'items' => array(
+          array(
+            'name' => 'Location',
+            'route' => $this->generateUrl('admin_location')
+          ),
+          array(
+            'name' => 'Group',
+            'route' => $this->generateUrl('admin_group')
+          ),
+          array(
+            'name' => 'Currency',
+            'route' => $this->generateUrl('admin_currency')
+          ),
+          array(
+            'name' => 'Ban',
+            'route' => $this->generateUrl('ban')
+          ),
+          array(
+            'name' => 'Task',
+            'route' => $this->generateUrl('admin_task')
+          ),
+          array(
+            'name' => 'Log',
+            'route' => $this->generateUrl('admin_log')
+          ),
+        )
+      ),
+      array(
+        'name' => 'Shop',
+        'route' => $this->generateUrl('shop'),
+        'items' => array(
+          array(
+            'name' => 'Product',
+            'route' => $this->generateUrl('admin_shop_product')
+          ),
+          array(
+            'name' => 'Category',
+            'route' => $this->generateUrl('admin_shop_category')
+          ),
+          array(
+            'name' => 'Order',
+            'route' => $this->generateUrl('admin_shop_order')
+          ),
+          array(
+            'name' => 'Payment Method',
+            'route' => $this->generateUrl('admin_shop_payment_method')
+          ),
+          array(
+            'name' => 'Shipping',
+            'route' => $this->generateUrl('admin_shop_shipping')
+          ),
+        )
+      ),
+      array(
+        'name' => 'Event',
+        'route' => $this->generateUrl('admin_event_event'),
+        'items' => array()
+      ),
+      array(
+        'name' => 'Account',
+        'route' => $this->generateUrl('club_account_adminaccount_index'),
+        'items' => array()
+      )
+    );
+
+    return $this->render('ClubMenuBundle:Default:leftMenu.html.twig', array(
+      'menu' => $menu
+    ));
+  }
 }
