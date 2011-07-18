c18cf44ed9631640a0b5f1dcadd6704533832f4b
Michael Holm <hollo@hollo.dk>
2011-07-18 13:40:54 +0200

    Updated documentation.

--- src/Club/remake_schema.sh
+++ src/Club/remake_schema.sh
@@ -3,8 +3,6 @@
 sudo rm -rf ../../app/cache/*
 sudo chmod 777 ../../app/logs ../../app/cache
 
-#php ../../bin/vendors install
-
 php ../../app/console doctrine:database:drop --force
 php ../../app/console doctrine:database:create
 php ../../app/console doctrine:migrations:migrate --no-interaction
@@ -13,8 +11,8 @@ php ../../app/console assets:install ../../web
 
 mysql -u root clubmaster_v2 < Docs/sql/test_data.sql
 mysql -u root clubmaster_v2 < Docs/sql/users_data.sql
+mysql -u root clubmaster_v2 < Docs/sql/order_data.sql
 
-touch ../../app/installer
 php ../../app/console cache:warmup
 
 sudo chmod 777 -R ../../app/logs ../../app/cache
