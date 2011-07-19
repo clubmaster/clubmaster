d719b7589440e2184bbc09e09cbd685b2b33872c
Michael Holm <hollo@hollo.dk>
2011-07-19 08:26:34 +0200

    Fixed broken fixtures.

--- src/Club/UserBundle/DataFixtures/ORM/LoadLocationData.php
+++ src/Club/UserBundle/DataFixtures/ORM/LoadLocationData.php
@@ -34,13 +34,6 @@ class LoadLocationData extends AbstractFixture implements OrderedFixtureInterfac
     $lc->setValue(2);
     $manager->persist($lc);
 
-    $config = $this->getReference('config_account_default_vat');
-    $lc = new \Club\UserBundle\Entity\LocationConfig();
-    $lc->setLocation($location);
-    $lc->setConfig($config->getConfigKey());
-    $lc->setValue(2);
-    $manager->persist($lc);
-
     $config = $this->getReference('config_default_language');
     $lc = new \Club\UserBundle\Entity\LocationConfig();
     $lc->setLocation($location);
