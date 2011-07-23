5d4e64977ea2de79b22ef6fe7808199673825841
Michael Holm <hollo@hollo.dk>
2011-07-23 12:39:29 +0200

    Cannot add categories to root location.

--- src/Club/ShopBundle/Form/Category.php
+++ src/Club/ShopBundle/Form/Category.php
@@ -4,6 +4,7 @@ namespace Club\ShopBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Doctrine\ORM\EntityRepository;
 
 class Category extends AbstractType
 {
@@ -11,8 +12,17 @@ class Category extends AbstractType
   {
     $builder->add('category_name');
     $builder->add('description');
-    $builder->add('category');
-    $builder->add('location');
+    $builder->add('category','entity',array(
+      'class' => 'Club\ShopBundle\Entity\Category',
+      'required' => false
+    ));
+    $builder->add('location','entity',array(
+      'class' => 'Club\UserBundle\Entity\Location',
+      'query_builder' => function(EntityRepository $er) {
+        return $er->createQueryBuilder('l')
+          ->where('l.id <> 1');
+      }
+    ));
   }
 
   public function getDefaultOptions(array $options)
