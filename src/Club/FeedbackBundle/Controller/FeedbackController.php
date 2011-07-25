6a67d7ba3157a404539444f59f24147a1ec69b93
Michael Holm <hollo@hollo.dk>
2011-07-25 20:20:27 +0200

    Added prettier form of feedback.

--- src/Club/FeedbackBundle/Controller/FeedbackController.php
+++ src/Club/FeedbackBundle/Controller/FeedbackController.php
@@ -14,6 +14,31 @@ class FeedbackController extends Controller
    */
   public function indexAction()
   {
-    return array();
+    $type = array(
+      'idea' => $this->get('translator')->trans('Idea'),
+      'bug' => $this->get('translator')->trans('Bug'),
+      'other' => $this->get('translator')->trans('Other')
+    );
+
+    $form = $this->createFormBuilder()
+      ->add('type','choice',array(
+        'choices' => $type
+      ))
+      ->add('subject','text', array(
+        'required' => true
+      ))
+      ->add('message','textarea')
+      ->getForm();
+
+    if ($this->getRequest()->getMethod() == 'POST') {
+      $form->bindRequest($this->getRequest());
+
+      if ($form->isValid()) {
+        $res = $form->getData();
+      }
+    }
+    return array(
+      'form' => $form->createView()
+    );
   }
 }
