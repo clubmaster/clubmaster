cd75f06c217004f6e8762c3fa3963fac68eeab52
Michael Holm <hollo@hollo.dk>
2011-07-26 09:05:33 +0200

    Corrected hostname.

--- src/Club/FeedbackBundle/Controller/FeedbackController.php
+++ src/Club/FeedbackBundle/Controller/FeedbackController.php
@@ -37,8 +37,6 @@ class FeedbackController extends Controller
         $data = $form->getData();
         $this->sendData($data);
 
-        $this->get('session')->setFlash('notice',$this->get('translator')->trans('Your message has been sent.'));
-
         return $this->redirect($this->generateUrl('club_feedback_feedback_index'));
       }
     }
@@ -49,25 +47,33 @@ class FeedbackController extends Controller
 
   private function sendData(array $data)
   {
-    $host = 'dev.hollo.dk';
+    $host = 'loopback.clubmaster.dk';
+    $fp = @fsockopen($host,80);
 
-    $fp = fsockopen("dev.hollo.dk",80);
+    $str = '';
+    foreach ($data as $key=>$value) {
+      $str .= $key.'='.$value.'&';
+    }
 
-    $str = "id=14";
     if ($fp) {
-      fputs($fp, "POST /backend/web/app_dev.php/feedback HTTP/1.1\r\n");
-      fputs($fp, "From: mh@clubmaster.dk\r\n");
-      fputs($fp, "User-Agent: HTTPTool 1.0\r\n");
-      fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
-      fputs($fp, "Content-length: ".strlen($str)."\r\n\r\n");
+      fputs($fp, "POST /feedback HTTP/1.1\r\n");
+      fputs($fp, "HOST: loopback.clubmaster.dk\r\n");
+      fputs($fp, "User-Agent: ClubMasterTool 1.0\r\n");
+      fputs($fp, "Content-length: ".strlen($str)."\r\n");
+      fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n\r\n");
 
       fputs($fp, $str."\r\n\r\n");
 
-      $d = '';
-      while (!feof($fp)) $d .= fgets($fp,4096);
+      $res = '';
+      while (!feof($fp)) $res .= fgets($fp,4096);
       fclose($fp);
 
-      var_dump($d);die();
+      if (strpos($res,'200 OK')) {
+        $this->get('session')->setFlash('notice',$this->get('translator')->trans('Your message has been sent.'));
+        return;
+      }
     }
+
+    $this->get('session')->setFlash('error',$this->get('translator')->trans('There was a problem delivering your message.'));
   }
 }
