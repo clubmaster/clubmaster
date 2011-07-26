2f8a73d144989e3b967776f9852e06edad562dc7
Michael Holm <hollo@hollo.dk>
2011-07-26 12:16:57 +0200

    Renamed controller for communication bundle.

--- src/Club/CommunicationBundle/Controller/AdminCommunicationController.php
+++ src/Club/CommunicationBundle/Controller/AdminCommunicationController.php
@@ -5,9 +5,9 @@ namespace Club\CommunicationBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
-class DefaultController extends Controller
+class AdminCommunicationController extends Controller
 {
-    
+
     public function indexAction($name)
     {
         return $this->render('ClubCommunicationBundle:Default:index.html.twig', array('name' => $name));
