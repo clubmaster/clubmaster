4f8c64f4d8beac966e7bfbdc8e313e3c198f594e
Michael Holm <hollo@hollo.dk>
2011-09-26 15:21:13 +0200

    Improved builder file.

--- bin/build.sh
+++ bin/build.sh
@@ -12,6 +12,7 @@ php app/console doctrine:database:create
 php app/console doctrine:migrations:migrate --no-interaction
 php app/console doctrine:fixtures:load
 SQL_DUMP_FILE=`mktemp`
+chmod 644 ${SQL_DUMP_FILE}
 mysqldump -u root clubmaster > ${SQL_DUMP_FILE}
 
 mkdir ${BUILD_PATH}
@@ -23,11 +24,12 @@ cp bin/install.sh ${BUILD_PATH}
 
 rm -rf ${BUILD_PATH}/bin
 rm -rf ${BUILD_PATH}/deps
-rm -rf ${BUILD_PATH}/deps.lock
 rm -rf ${BUILD_PATH}/app/cache/*
-rm -rf ${BUILD_PATH}/app/phpunit.xml.dist
 rm -rf ${BUILD_PATH}/app/logs/*
+rm -rf ${BUILD_PATH}/app/spool/.*.message
 rm -rf ${BUILD_PATH}/app/sql/*
+rm -rf ${BUILD_PATH}/deps.lock
+rm -rf ${BUILD_PATH}/app/phpunit.xml.dist
 rm -rf ${BUILD_PATH}/web/index_dev.php
 
 cp ${SQL_DUMP_FILE} ${BUILD_PATH}/app/sql/install.sql
@@ -43,6 +45,7 @@ cat > ${BUILD_PATH}/app/config/parameters.ini <<EOF
 [parameters]
     database_driver   = pdo_mysql
     database_host     = localhost
+    database_port     =
     database_name     = clubmaster
     database_user     = root
     database_password =
