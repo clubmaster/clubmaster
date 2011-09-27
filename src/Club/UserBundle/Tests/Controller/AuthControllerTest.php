0426affa2c1027e3b940f3d34665595398adb921
Michael Holm <hollo@hollo.dk>
2011-09-27 10:46:35 +0200

    Added Tests for user bundle.

---
+++ src/Club/UserBundle/Tests/Controller/AuthControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Club\UserBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DemoControllerTest extends WebTestCase
+{
+  public function testIndex()
+  {
+    $client = static::createClient();
+    $crawler = $client->request('GET', '/auth/register');
+    $this->assertTrue($crawler->filter('html:contains("Personal Information")')->count() > 0);
+  }
+}
