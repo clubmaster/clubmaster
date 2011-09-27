165a53793c229ef285dcaacf173cd91a724f9cfb
Michael Holm <hollo@hollo.dk>
2011-09-27 13:40:44 +0200

    Added checkout tests.

--- src/Club/ShopBundle/Tests/Controller/CheckoutControllerTest.php
+++ src/Club/ShopBundle/Tests/Controller/CheckoutControllerTest.php
@@ -22,6 +22,39 @@ class CheckoutControllerTest extends WebTestCase
     $link = $crawler->selectLink('Empty cart')->link();
     $crawler = $client->click($link);
     $this->assertEquals(302, $client->getResponse()->getStatusCode());
-
   }
+
+  public function testCheckout()
+  {
+    $client = static::createClient(array(), array(
+      'PHP_AUTH_USER' => '10',
+      'PHP_AUTH_PW' => '1234'
+    ));
+    $crawler = $client->request('GET', '/shop/product/1');
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    $link = $crawler->selectLink('Put in cart')->link();
+    $crawler = $client->click($link);
+    $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    $crawler = $client->followRedirect();
+
+    $link = $crawler->selectLink('Checkout')->link();
+    $crawler = $client->click($link);
+    $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    $crawler = $client->followRedirect();
+
+    $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    $crawler = $client->followRedirect();
+
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    $form = $crawler->selectButton('Confirm order')->form();
+    $crawler = $client->submit($form);
+
+    $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+    #$link = $crawler->selectLink('Confirm order')->link();
+    #$crawler = $client->click($link);
+    #$this->assertEquals(302, $client->getResponse()->getStatusCode());
+  }
+
 }
