497c242273fcb63f66aabf5aabdd5dc7c1c10f22
Michael Holm <hollo@hollo.dk>
2011-09-28 17:45:37 +0200

    Fixed request password form.

--- src/Club/UserBundle/Form/RequestPassword.php
+++ src/Club/UserBundle/Form/RequestPassword.php
@@ -9,11 +9,11 @@ class RequestPassword extends AbstractType
 {
   public function buildForm(FormBuilder $builder, array $options)
   {
-    $builder->add('username', 'text',array(
-      'required' => 'false'
+    $builder->add('username', 'text', array(
+      'required' => false
     ));
-    $builder->add('email', 'text',array(
-      'required' => 'false'
+    $builder->add('email', 'text', array(
+      'required' => false
     ));
   }
 
