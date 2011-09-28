ca45683c8b2177f6ebb9e37387d807c87a9443eb
Michael Holm <hollo@hollo.dk>
2011-09-28 14:05:29 +0200

    Added create event to tests.

--- src/Club/EventBundle/Tests/Controller/EventControllerTest.php
+++ src/Club/EventBundle/Tests/Controller/EventControllerTest.php
@@ -23,10 +23,26 @@ class EventControllerTest extends WebTestCase
     $this->assertEquals(200, $client->getResponse()->getStatusCode());
   }
 
-  public function testical()
+  public function testICal()
   {
     $client = static::createClient();
     $crawler = $client->request('GET', '/event/ical');
     $this->assertEquals(200, $client->getResponse()->getStatusCode());
+  }
+
+  public function testNew()
+  {
+    $client = static::createClient();
+    $this->login($client);
+
+    $crawler = $client->request('GET', '/admin/event/event/new');
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    $form = $crawler->selectButton('Save')->form(array(
+      'event[event_name]' => 'Test',
+      'event[description]' => 'Test'
+    ));
+    $crawler = $client->submit($form);
+    $this->assertEquals(302, $client->getResponse()->getStatusCode());
   }
 }
