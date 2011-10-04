81ddef5e77120eae509acce720d573cc107b8914
Michael Holm <hollo@hollo.dk>
2011-10-04 17:21:06 +0200

    Added more tests.

--- src/Club/UserBundle/Tests/Controller/AuthControllerTest.php
+++ src/Club/UserBundle/Tests/Controller/AuthControllerTest.php
@@ -5,10 +5,25 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AuthControllerTest extends WebTestCase
 {
-  public function testIndex()
+  public function testRegister()
   {
     $client = static::createClient();
     $crawler = $client->request('GET', '/auth/register');
-    $this->assertTrue($crawler->filter('html:contains("Personal Information")')->count() > 0);
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    $form = $crawler->selectButton('Save')->form();
+    $crawler = $client->submit($form);
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    $form['user[profile][first_name]'] = 'John';
+    $form['user[profile][last_name]'] = 'Doe';
+    $form['user[profile][profile_address][street]'] = 'Myllerstrasse 14';
+    $form['user[profile][profile_address][postal_code]'] = '9000';
+    $form['user[profile][profile_address][city]'] = 'Aalborg';
+    $form['user[profile][profile_email][email_address]'] = 'user@example.com';
+    $form['user[profile][profile_phone][number]'] = '80808080';
+
+    $crawler = $client->submit($form);
+    $this->assertEquals(302, $client->getResponse()->getStatusCode());
   }
 }
