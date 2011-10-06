42f68a11c69f62002320c3da08be428ccee20574
Michael Holm <hollo@hollo.dk>
2011-10-06 13:45:33 +0200

    Improved group test.

--- src/Club/UserBundle/Tests/Controller/AdminGroupControllerTest.php
+++ src/Club/UserBundle/Tests/Controller/AdminGroupControllerTest.php
@@ -8,9 +8,10 @@ class AdminGroupControllerTest extends WebTestCase
   protected function login($client)
   {
     $crawler = $client->request('GET', '/login');
-    $form = $crawler->selectButton('Sign In')->form();
-    $form['_username'] = '10';
-    $form['_password'] = '1234';
+    $form = $crawler->selectButton('Sign In')->form(array(
+      '_username' => '10',
+      '_password' => '1234'
+    ));
     $crawler = $client->submit($form);
   }
 
@@ -29,6 +30,10 @@ class AdminGroupControllerTest extends WebTestCase
     $this->login($client);
 
     $crawler = $client->request('GET', '/admin/group/new');
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    $form = $crawler->selectButton('Save')->form();
+    $crawler = $client->submit($form);
     $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
     $form = $crawler->selectButton('Save')->form(array(
