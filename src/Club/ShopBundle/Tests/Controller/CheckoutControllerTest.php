c64e19a06fcee21a3ab7c541ce6d5a147bfb2b5b
Michael Holm <hollo@hollo.dk>
2011-10-06 12:23:35 +0200

    Added subscription handling tests.

--- src/Club/ShopBundle/Tests/Controller/CheckoutControllerTest.php
+++ src/Club/ShopBundle/Tests/Controller/CheckoutControllerTest.php
@@ -37,7 +37,7 @@ class CheckoutControllerTest extends WebTestCase
     $client = static::createClient();
     $this->login($client);
 
-    $crawler = $client->request('GET', '/shop/product/1');
+    $crawler = $client->request('GET', '/shop/product/11');
     $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
     $link = $crawler->selectLink('Put in cart')->link();
@@ -57,6 +57,42 @@ class CheckoutControllerTest extends WebTestCase
     $form = $crawler->selectButton('Confirm order')->form();
     $crawler = $client->submit($form);
 
+    $this->assertEquals(302, $client->getResponse()->getStatusCode());
+  }
+
+  public function testOrder()
+  {
+    $client = static::createClient();
+    $this->login($client);
+
+    $crawler = $client->request('GET', '/admin/shop/order');
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    $links = $crawler->selectLink('Edit')->links();
+    $crawler = $client->click($links[0]);
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    $form = $crawler->selectButton('Save')->form(array(
+      'order[order_status]' => '4'
+    ));
+    $crawler = $client->submit($form);
+    $this->assertEquals(302, $client->getResponse()->getStatusCode());
+  }
+
+  public function testSubscription()
+  {
+    $client = static::createClient();
+    $this->login($client);
+
+    $crawler = $client->request('GET', '/shop/subscription');
+    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    $link = $crawler->selectLink('Pause')->link();
+    $crawler = $client->click($link);
+    $this->assertEquals(302, $client->getResponse()->getStatusCode());
+
+    $link = $crawler->selectLink('Resume')->link();
+    $crawler = $client->click($link);
     $this->assertEquals(302, $client->getResponse()->getStatusCode());
   }
 }
