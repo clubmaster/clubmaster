52499b4824e6f693edaed50de64dc013cb1d5603
Michael Holm <hollo@hollo.dk>
2011-10-06 14:46:27 +0200

    Improved task controller.

--- src/Club/TaskBundle/Tests/Controller/TaskControllerTest.php
+++ src/Club/TaskBundle/Tests/Controller/TaskControllerTest.php
@@ -1,25 +1,21 @@
 <?php
 namespace Club\TaskBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Club\UserBundle\Helper\TestCase as WebTestCase;
 
 class TaskControllerTest extends WebTestCase
 {
-  protected function login($client)
+  protected $client;
+
+  public function __construct()
   {
-    $crawler = $client->request('GET', '/login');
-    $form = $crawler->selectButton('Sign In')->form();
-    $form['_username'] = '10';
-    $form['_password'] = '1234';
-    $crawler = $client->submit($form);
+    $this->client = static::createClient();
+    $this->login($this->client);
   }
 
   public function testIndex()
   {
-    $client = static::createClient();
-    $this->login($client);
-
-    $crawler = $client->request('GET', '/admin/task');
-    $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    $crawler = $this->client->request('GET', '/admin/task');
+    $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
   }
 }
