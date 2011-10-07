26b19968ca43b5c91e91c809920f80fa3a335c79
Michael Holm <hollo@hollo.dk>
2011-10-07 13:39:27 +0200

    Added 10 min subscription to test data.

--- app/sql/test_data.sql
+++ app/sql/test_data.sql
@@ -76,7 +76,8 @@ Praesent scelerisque aliquam purus, vitae accumsan erat bibendum ut. Curabitur a
   (16,'Subscription + 3Month + Renewal(A) + Start date in future','Lorem ipsum...',100,1),
   (17,'Subscription + 1Month + Start date + Renewal(Y)','Lorem ipsum...',100,1),
   (18,'Subscription + Start date + Expire date + Renewal(Y) + ealier this year','Lorem ipsum...',100,1),
-  (19,'Daily subscription','Lorem ipsum...',100,1);
+  (19,'Daily subscription','Lorem ipsum...',100,1),
+  (20,'10 min subscription','Lorem ipsum...',35,1);
 
 INSERT INTO club_shop_category_product (product_id,category_id) VALUES
   (1,1),
@@ -97,7 +98,8 @@ INSERT INTO club_shop_category_product (product_id,category_id) VALUES
   (16,1),
   (17,1),
   (18,1),
-  (19,1);
+  (19,1),
+  (20,1);
 
 INSERT INTO club_shop_product_attribute (product_id,attribute_id,value) VALUES
   (1,1,'1M'),
@@ -142,5 +144,6 @@ INSERT INTO club_shop_product_attribute (product_id,attribute_id,value) VALUES
   (18,6,'2011-06-12'),
   (18,3,'Y'),
   (19,1,'1D'),
-  (19,3,'A');
-
+  (19,3,'A'),
+  (20,1,'T10M'),
+  (20,3,'A');
