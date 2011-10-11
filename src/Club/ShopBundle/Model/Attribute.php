632df4ea52c6016910ce17a3d7a0aa3a0cf87aaa
Michael Holm <hollo@hollo.dk>
2011-10-11 15:50:08 +0200

    Make attribute constraints.

--- src/Club/ShopBundle/Model/Attribute.php
+++ src/Club/ShopBundle/Model/Attribute.php
@@ -3,7 +3,11 @@
 namespace Club\ShopBundle\Model;
 
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\ExecutionContext;
 
+/**
+ * @Assert\Callback(methods={"isValid"})
+ */
 class Attribute
 {
   /**
@@ -42,4 +46,14 @@ class Attribute
   public $expire_date;
 
   public $location;
+
+
+  public function isValid(ExecutionContext $context)
+  {
+    if ($this->auto_renewal == 'Y' && $this->time_interval != '')
+      $context->addViolation('You are not able to make yearly renewal with a time interval, choose a start date instead.', array(), null);
+
+    if ($this->expire_date != '' && $this->start_date == '')
+      $context->addViolation('You are not able to have an expire date without a start date.', array(), null);
+  }
 }
