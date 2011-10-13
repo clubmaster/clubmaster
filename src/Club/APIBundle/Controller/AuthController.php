95725e7fc3c3ab6cf1f422ca0a8583a4a0190fe2
Michael Holm <hollo@hollo.dk>
2011-10-13 15:51:42 +0200

    Added AuthController for API.

---
+++ src/Club/APIBundle/Controller/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Club\APIBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+class AuthController extends Controller
+{
+  /**
+   * @Route("/auth")
+   */
+  public function authAction()
+  {
+  }
+}
