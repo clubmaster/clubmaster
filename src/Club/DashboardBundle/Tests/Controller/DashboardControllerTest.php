2a9befba8368f736d011e324b497ef36f61dce05
Michael Holm <hollo@hollo.dk>
2011-10-16 15:56:18 +0200

    Added tests for dashboard.

---
+++ src/Club/DashboardBundle/Tests/Controller/DashboardControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Club\DashboardBundle\Tests\Controller;
+
+use Club\UserBundle\Helper\TestCase as WebTestCase;
+
+class DashboardControllerTest extends WebTestCase
+{
+  protected $client;
+
+  public function __construct()
+  {
+    $this->client = static::createClient();
+    $this->login($this->client);
+  }
+
+  public function testIndex()
+  {
+    $crawler = $this->client->request('GET', '/admin/dashboard');
+    $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+  }
+}
