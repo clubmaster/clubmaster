a9bf6de047789901de2473e72e75fb67f75ef3d0
Michael Holm <hollo@hollo.dk>
2011-11-12 00:16:25 +0100

    Simplified remake script.

--- bin/remake_schema.sh
+++ bin/remake_schema.sh
@@ -9,9 +9,13 @@ php app/console doctrine:migrations:migrate --no-interaction
 php app/console doctrine:fixtures:load
 php app/console assets:install web
 
-mysql -u root clubmaster < app/sql/test_data.sql
-mysql -u root clubmaster < app/sql/users_data.sql
-mysql -u root clubmaster < app/sql/event_data.sql
+MYSQL_PASSWORD=
+MYSQL_DATABASE=clubmaster
+
+mysql -u root -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/test_data.sql
+mysql -u root -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/users_data.sql
+mysql -u root -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/event_data.sql
+mysql -u root -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/team_data.sql
 
 php app/console cache:warmup
 
