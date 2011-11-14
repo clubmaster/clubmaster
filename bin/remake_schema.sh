c21f5c143b5776e096c04e3e697185e2bb91f855
Michael Holm <hollo@hollo.dk>
2011-11-14 12:54:42 +0100

    Improved remake script.

--- bin/remake_schema.sh
+++ bin/remake_schema.sh
@@ -9,13 +9,17 @@ php app/console doctrine:migrations:migrate --no-interaction
 php app/console doctrine:fixtures:load
 php app/console assets:install web
 
-MYSQL_PASSWORD=
+MYSQL_PASSWORD=""
 MYSQL_DATABASE=clubmaster
 
-mysql -u root -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/test_data.sql
-mysql -u root -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/users_data.sql
-mysql -u root -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/event_data.sql
-mysql -u root -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/team_data.sql
+if [ "${MYSQL_PASSWORD}" != "" ]; then
+  MYSQL_PASSWORD="-p"${MYSQL_PASSWORD}
+fi
+
+mysql -u root ${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/test_data.sql
+mysql -u root ${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/users_data.sql
+mysql -u root ${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/event_data.sql
+mysql -u root ${MYSQL_PASSWORD} ${MYSQL_DATABASE} < app/sql/team_data.sql
 
 php app/console cache:warmup
 
