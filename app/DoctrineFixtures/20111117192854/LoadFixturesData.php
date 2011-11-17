29d0b3d9fba10cce9f96ed32e8c4b322dba82d36
Michael Holm <hollo@hollo.dk>
2011-11-17 20:55:27 +0100

    Added data fixtures for upgrade.

---
+++ app/DoctrineFixtures/20111117192854/LoadFixturesData.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Application\Fixtures;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+
+class LoadFixturesData implements FixtureInterface
+{
+  public function load($manager)
+  {
+    $task = new \Club\TaskBundle\Entity\Task();
+    $task->setTaskName('Generate new team schedules');
+    $task->setEnabled(1);
+    $task->setLocked(0);
+    $task->setNextRunAt(new \DateTime());
+    $task->setTaskInterval('1D');
+    $task->setEvent('\Club\TaskBundle\Event\Events');
+    $task->setMethod('onTeamTask');
+    $manager->persist($task);
+
+    $task = new \Club\TaskBundle\Entity\Task();
+    $task->setTaskName('Create team penalty');
+    $task->setEnabled(1);
+    $task->setLocked(0);
+    $task->setNextRunAt(new \DateTime());
+    $task->setTaskInterval('1D');
+    $task->setEvent('\Club\TaskBundle\Event\Events');
+    $task->setMethod('onTeamPenalty');
+    $manager->persist($task);
+
+    $roles = array();
+    $roles[] = 'ROLE_STAFF';
+
+    foreach ($roles as $role) {
+      $r = new \Club\UserBundle\Entity\Role();
+      $r->setRoleName($role);
+
+      $this->addReference($role,$r);
+      $manager->persist($r);
+    }
+
+    $attributes = array();
+    $attributes[] = 'team';
+
+    foreach ($attributes as $attr) {
+      $a = new \Club\ShopBundle\Entity\Attribute();
+      $a->setAttributeName($attr);
+
+      $manager->persist($a);
+    }
+
+    $group = new \Club\UserBundle\Entity\Group();
+    $group->setGroupName('Staff');
+    $group->setGroupType('static');
+    $group->addRole($this->getReference('ROLE_STAFF'));
+    $manager->persist($group);
+
+    $manager->flush();
+  }
+}
