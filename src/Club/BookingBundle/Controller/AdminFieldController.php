e075d050bdd0c46244d574332be9e181e6f00252
Michael Holm <hollo@hollo.dk>
2012-01-10 15:18:21 +0100

    Added CRUD for booking field module.

--- src/Club/BookingBundle/Controller/AdminFieldController.php
+++ src/Club/BookingBundle/Controller/AdminFieldController.php
@@ -3,19 +3,96 @@
 namespace Club\BookingBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class AdminFieldController extends Controller
 {
-   /**
-    * @Template()
-    * @Route("/field")
-    */
-   public function indexAction($interval_id,$date)
-   {
-     return array();
-   }
+  /**
+   * @Route("/booking/field")
+   * @Template()
+   */
+  public function indexAction()
+  {
+    $em = $this->getDoctrine()->getEntityManager();
+    $fields = $em->getRepository('ClubBookingBundle:Field')->findAll();
+
+    return array(
+      'fields' => $fields
+    );
+  }
+
+  /**
+   * @Route("/booking/field/new")
+   * @Template()
+   */
+  public function newAction()
+  {
+    $field = new \Club\BookingBundle\Entity\Field();
+    $res = $this->process($field);
+
+    if ($res instanceOf RedirectResponse)
+      return $res;
+
+    return array(
+      'form' => $res->createView()
+    );
+  }
+
+  /**
+   * @Route("/booking/field/edit/{id}")
+   * @Template()
+   */
+  public function editAction($id)
+  {
+    $em = $this->getDoctrine()->getEntityManager();
+    $field = $em->find('ClubBookingBundle:Field',$id);
+
+    $res = $this->process($field);
+
+    if ($res instanceOf RedirectResponse)
+      return $res;
+
+    return array(
+      'field' => $field,
+      'form' => $res->createView()
+    );
+  }
+
+  /**
+   * @Route("/booking/field/delete/{id}")
+   */
+  public function deleteAction($id)
+  {
+    $em = $this->getDoctrine()->getEntityManager();
+    $field = $em->find('ClubBookingBundle:Field',$this->getRequest()->get('id'));
+
+    $em->remove($field);
+    $em->flush();
+
+    $this->get('session')->setFlash('notice',$this->get('translator')->trans('Your changes are saved.'));
+
+    return $this->redirect($this->generateUrl('club_booking_adminfield_index'));
+  }
+
+  protected function process($field)
+  {
+    $form = $this->createForm(new \Club\BookingBundle\Form\Field(), $field);
+
+    if ($this->getRequest()->getMethod() == 'POST') {
+      $form->bindRequest($this->getRequest());
+      if ($form->isValid()) {
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($field);
+        $em->flush();
+
+        $this->get('session')->setFlash('notice',$this->get('translator')->trans('Your changes are saved.'));
+
+        return $this->redirect($this->generateUrl('club_booking_adminfield_index'));
+      }
+    }
+
+    return $form;
+  }
 }
---
+++ src/Club/BookingBundle/Form/Field.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Club\BookingBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Doctrine\ORM\EntityRepository;
+
+class Field extends AbstractType
+{
+  public function buildForm(FormBuilder $builder, array $options)
+  {
+    $builder->add('name');
+    $builder->add('position');
+    $builder->add('information');
+    $builder->add('location','entity',array(
+      'class' => 'Club\UserBundle\Entity\Location',
+      'query_builder' => function(EntityRepository $er) {
+        return $er->createQueryBuilder('l')
+          ->where('l.id <> 1');
+      }
+    ));
+  }
+
+  public function getDefaultOptions(array $options)
+  {
+    return array(
+      'data_class' => 'Club\BookingBundle\Entity\Field'
+    );
+  }
+
+  public function getName()
+  {
+    return 'admin_field';
+  }
+}
