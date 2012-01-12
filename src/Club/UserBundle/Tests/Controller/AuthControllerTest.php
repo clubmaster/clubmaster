f232f0ea37246e20a0095d4e86219b8a58e1cf0c
Michael Holm <hollo@hollo.dk>
2012-01-12 14:01:50 +0100

    Added test for forgot password.

--- src/Club/UserBundle/Tests/Controller/AuthControllerTest.php
+++ src/Club/UserBundle/Tests/Controller/AuthControllerTest.php
@@ -11,18 +11,8 @@ class AuthControllerTest extends WebTestCase
     $crawler = $client->request('GET', '/auth/forgot');
     $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
-    $form = $crawler->selectButton('Save')->form();
-    $crawler = $client->submit($form);
-    $this->assertEquals(200, $client->getResponse()->getStatusCode());
-
-    $form = $crawler->selectButton('Save')->form(array(
-      'request_password[username]' => '10',
-      'request_password[email]' => 'Doe',
-      'user[profile][profile_address][street]' => 'Myllerstrasse 14',
-      'user[profile][profile_address][postal_code]' => '9000',
-      'user[profile][profile_address][city]' => 'Aalborg',
-      'user[profile][profile_email][email_address]' => 'user@example.com',
-      'user[profile][profile_phone][phone_number]' => '80808080'
+    $form = $crawler->selectButton('Send Password')->form(array(
+      'form[username]' => '10',
     ));
     $crawler = $client->submit($form);
     $this->assertEquals(302, $client->getResponse()->getStatusCode());
