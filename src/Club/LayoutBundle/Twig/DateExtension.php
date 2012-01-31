b87da8653bd02f3a156d3014a243dad39dbb97f9
Michael Holm <hollo@hollo.dk>
2012-01-31 10:26:36 +0100

    Rewritten all data filters to club_date filter.

--- src/Club/LayoutBundle/Twig/DateExtension.php
+++ src/Club/LayoutBundle/Twig/DateExtension.php
@@ -7,22 +7,26 @@ class DateExtension extends \Twig_Extension
   private $em;
   private $security_context;
   private $session;
+  private $locale;
 
   public function __construct($em, $security_context, $session)
   {
     $this->em = $em;
     $this->security_context = $security_context;
     $this->session = $session;
+    $this->locale = $this->session->getLocale();
   }
 
   public function getFilters()
   {
     return array(
-      'club_date' => new \Twig_Filter_Method($this, 'getDate')
+      'club_date' => new \Twig_Filter_Method($this, 'getDate'),
+      'club_datetime' => new \Twig_Filter_Method($this, 'getDateTime'),
+      'club_time' => new \Twig_Filter_Method($this, 'getTime'),
     );
   }
 
-  public function getDate($value, $type='MEDIUM')
+  public function getDate($value, $type='SHORT')
   {
     $type = strtoupper($type);
     switch ($type) {
@@ -39,7 +43,53 @@ class DateExtension extends \Twig_Extension
       $date = \IntlDateFormatter::MEDIUM;
       break;
     }
-    $fmt = new \IntlDateFormatter($this->session->getLocale(), $date, \IntlDateFormatter::NONE);
+    $fmt = new \IntlDateFormatter($this->locale, $date, \IntlDateFormatter::NONE);
+    return $fmt->format($value);
+  }
+
+  public function getDateTime($value, $type='SHORT')
+  {
+    $type = strtoupper($type);
+    switch ($type) {
+    case 'FULL':
+      $date = \IntlDateFormatter::FULL;
+      $time = \IntlDateFormatter::FULL;
+      break;
+    case 'LONG':
+      $date = \IntlDateFormatter::LONG;
+      $time = \IntlDateFormatter::LONG;
+      break;
+    case 'SHORT':
+      $date = \IntlDateFormatter::SHORT;
+      $time = \IntlDateFormatter::SHORT;
+      break;
+    default:
+      $date = \IntlDateFormatter::MEDIUM;
+      $time = \IntlDateFormatter::MEDIUM;
+      break;
+    }
+    $fmt = new \IntlDateFormatter($this->locale, $date, $time);
+    return $fmt->format($value);
+  }
+
+  public function getTime($value, $type='SHORT')
+  {
+    $type = strtoupper($type);
+    switch ($type) {
+    case 'FULL':
+      $time = \IntlDateFormatter::FULL;
+      break;
+    case 'LONG':
+      $time = \IntlDateFormatter::LONG;
+      break;
+    case 'SHORT':
+      $time = \IntlDateFormatter::SHORT;
+      break;
+    default:
+      $time = \IntlDateFormatter::MEDIUM;
+      break;
+    }
+    $fmt = new \IntlDateFormatter($this->locale, \IntlDateFormatter::NONE, $time);
     return $fmt->format($value);
   }
 
