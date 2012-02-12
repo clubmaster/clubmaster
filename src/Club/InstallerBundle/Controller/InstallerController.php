c8ea9830bbb0e9d9783d5deb5aa6f86d6160a30d
Michael Holm <hollo@hollo.dk>
2012-02-12 14:06:01 +0100

    Merged migrations into a new trunk.

--- src/Club/InstallerBundle/Controller/InstallerController.php
+++ src/Club/InstallerBundle/Controller/InstallerController.php
@@ -31,6 +31,24 @@ class InstallerController extends Controller
    * @Route("/step/1")
    * @Template()
    */
+  public function databaseAction()
+  {
+    try {
+      $configuration = $this->getMigrationsConfiguration();
+      $current = $configuration->getCurrentVersion();
+    } catch (\Exception $e) {
+      $this->get('session')->setFlash('error', $e->getMessage());
+    }
+
+    return array(
+      'configuration' => $configuration
+    );
+  }
+
+  /**
+   * @Route("/step/2")
+   * @Template()
+   */
   public function administratorAction()
   {
     $em = $this->getDoctrine()->getEntityManager();
@@ -75,7 +93,7 @@ class InstallerController extends Controller
   }
 
   /**
-   * @Route("/step/2")
+   * @Route("/step/3")
    * @Template()
    */
   public function locationAction()
@@ -114,11 +132,76 @@ class InstallerController extends Controller
   }
 
   /**
-   * @Route("/step/3")
+   * @Route("/step/4")
    * @Template()
    */
   public function confirmAction()
   {
     return array();
   }
+
+  /**
+   * @Route("/migrate")
+   * @Template()
+   */
+  public function migrateAction()
+  {
+    $this->migrate();
+
+    $this->get('session')->setFlash('notice', 'Database was successful installed');
+    return $this->redirect($this->generateUrl('club_installer_installer_administrator'));
+  }
+
+  private function getMigrationsConfiguration()
+  {
+    $em = $this->getDoctrine()->getEntityManager();
+
+    $dir = $this->container->getParameter('doctrine_migrations.dir_name');
+    $configuration = new \Doctrine\DBAL\Migrations\Configuration\Configuration($em->getConnection());
+    $configuration->setMigrationsNamespace($this->container->getParameter('doctrine_migrations.namespace'));
+    $configuration->setMigrationsDirectory($dir);
+    $configuration->registerMigrationsFromDirectory($dir);
+    $configuration->setName($this->container->getParameter('doctrine_migrations.name'));
+    $configuration->setMigrationsTableName($this->container->getParameter('doctrine_migrations.table_name'));
+
+    return $configuration;
+  }
+
+  private function migrate()
+  {
+    $em = $this->getDoctrine()->getEntityManager();
+
+    $configuration = $this->getMigrationsConfiguration();
+    $migration = new \Doctrine\DBAL\Migrations\Migration($configuration);
+
+    $from = $configuration->getCurrentVersion();
+    $to = $configuration->getLatestVersion();
+    $migrations = $configuration->getMigrations();
+
+    foreach ($migrations as $version) {
+      if ($version->getVersion() > $from) {
+        $migration->migrate($version->getVersion());
+        $this->loadFixtures($version->getVersion());
+      }
+    }
+  }
+
+  private function loadFixtures($version)
+  {
+    $em = $this->getDoctrine()->getEntityManager();
+    $dir = $this->get('kernel')->getRootDir().'/DoctrineFixtures/'.$version;
+
+    if (!file_exists($dir))
+      return;
+
+    $loader = new \Symfony\Bundle\DoctrineFixturesBundle\Common\DataFixtures\Loader($this->container);
+    $loader->loadFromDirectory($dir);
+    $fixtures = $loader->getFixtures();
+
+    $purger = new  \Doctrine\Common\DataFixtures\Purger\ORMPurger($em);
+    $executor = new \Doctrine\Common\DataFixtures\Executor\ORMExecutor($em, $purger);
+
+    $executor->execute($fixtures, true);
+  }
+
 }
