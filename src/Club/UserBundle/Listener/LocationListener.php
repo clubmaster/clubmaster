a15f2cb710106e111f8eec202ceffa2fa8ab1426
Michael Holm <hollo@hollo.dk>
2012-02-13 16:28:48 +0100

    Added location listener, and fixed signup form.

---
+++ src/Club/UserBundle/Listener/LocationListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Club\UserBundle\Listener;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class LocationListener
+{
+  protected $em;
+  protected $session;
+
+  public function __construct($em, $session)
+  {
+    $this->em = $em;
+    $this->session = $session;
+  }
+
+  public function onKernelRequest()
+  {
+    $this->setLocation();
+  }
+
+  private function setLocation()
+  {
+    if ($this->session->get('location_id')) return;
+
+    $location = $this->em->createQueryBuilder()
+      ->select('l')
+      ->from('ClubUserBundle:Location', 'l')
+      ->where('l.id > 1')
+      ->orderBy('l.id')
+      ->setMaxResults(1)
+      ->getQuery()
+      ->getOneOrNullResult();
+
+    $this->session->set('location_id', $location->getId());
+  }
+}
