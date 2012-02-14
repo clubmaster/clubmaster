80350ce46bc00c38f52e961546d7f0f152df54b0
Michael Holm <hollo@hollo.dk>
2012-02-14 15:26:29 +0100

    Only add shops in clubs.

--- src/Club/ShopBundle/Form/Category.php
+++ src/Club/ShopBundle/Form/Category.php
@@ -4,6 +4,7 @@ namespace Club\ShopBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Doctrine\ORM\EntityRepository;
 
 class Category extends AbstractType
 {
@@ -15,7 +16,14 @@ class Category extends AbstractType
       'class' => 'Club\ShopBundle\Entity\Category',
       'required' => false
     ));
-    $builder->add('location');
+    $builder->add('location','entity',array(
+      'class' => 'Club\UserBundle\Entity\Location',
+      'required' => false,
+      'query_builder' => function(EntityRepository $er) {
+        return $er->createQueryBuilder('l')
+          ->where('l.club = 1');
+      }
+    ));
   }
 
   public function getDefaultOptions(array $options)
