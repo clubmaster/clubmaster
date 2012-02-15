0d32cd09135c2ab7f8133b04722940be19e0f48a
Michael Holm <hollo@hollo.dk>
2012-02-15 11:55:38 +0100

    Fixed migrations dir.

--- bin/remake_schema.sh
+++ bin/remake_schema.sh
@@ -6,7 +6,7 @@ sudo chmod 777 app/logs app/cache
 php app/console doctrine:database:drop --force
 php app/console doctrine:database:create
 php app/console doctrine:migrations:migrate --no-interaction
-php app/console doctrine:fixtures:load --fixtures=app/DoctrineFixtures/20120214011048/
+php app/console doctrine:fixtures:load --fixtures=app/DoctrineFixtures/20120215114943/
 php app/console assets:install web
 
 MYSQL_PASSWORD=""
