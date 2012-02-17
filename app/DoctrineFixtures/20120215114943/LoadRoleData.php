e786d94871ada949280563287ece7aa41d1e7926
Michael Holm <hollo@hollo.dk>
2012-02-17 16:14:40 +0100

    Added role data.

--- app/DoctrineFixtures/20120215114943/LoadRoleData.php
+++ app/DoctrineFixtures/20120215114943/LoadRoleData.php
@@ -13,18 +13,22 @@ class LoadRoleData extends AbstractFixture implements OrderedFixtureInterface
     $roles = array();
     $roles[] = 'ROLE_SUPER_ADMIN';
     $roles[] = 'ROLE_ADMIN';
+    $roles[] = 'ROLE_BLOG_ADMIN';
+    $roles[] = 'ROLE_BOOKING_ADMIN';
     $roles[] = 'ROLE_EVENT_ADMIN';
+    $roles[] = 'ROLE_MEMBER_ADMIN';
+    $roles[] = 'ROLE_MESSAGE_ADMIN';
+    $roles[] = 'ROLE_RANKING_ADMIN';
+    $roles[] = 'ROLE_SHOP_ADMIN';
+    $roles[] = 'ROLE_TEAM_ADMIN';
     $roles[] = 'ROLE_STAFF';
 
     foreach ($roles as $role) {
       $r = new \Club\UserBundle\Entity\Role();
       $r->setRoleName($role);
-
       $this->addReference($role,$r);
-
       $manager->persist($r);
     }
-
     $manager->flush();
   }
 
