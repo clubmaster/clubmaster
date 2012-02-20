e7a1432f329aad30444f1fc2645baf3abc5cb12f
Michael Holm <hollo@hollo.dk>
2012-02-20 11:35:17 +0100

    Added game entity.

---
+++ src/Club/RankingBundle/Entity/Game.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Club\RankingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Club\RankingBundle\Entity\Game
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Club\RankingBundle\Entity\GameRepository")
+ * @ORM\HasLifeCycleCallbacks()
+ */
+class Game
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var boolean $locked
+     *
+     * @ORM\Column(type="boolean")
+     */
+    private $locked;
+
+    /**
+     * @var string $type
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var datetime $created_at
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @var datetime $updated_at
+     *
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_at;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Club\UserBundle\Entity\User", mappedBy="user")
+     * @ORM\JoinTable(name="club_ranking_game_user")
+     */
+    protected $users;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Club\UserBundle\Entity\User", mappedBy="user")
+     * @ORM\JoinTable(name="club_ranking_game_admins")
+     */
+    protected $administrators;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set locked
+     *
+     * @param boolean $locked
+     */
+    public function setLocked($locked)
+    {
+        $this->locked = $locked;
+    }
+
+    /**
+     * Get locked
+     *
+     * @return boolean
+     */
+    public function getLocked()
+    {
+        return $this->locked;
+    }
+
+    /**
+     * Set created_at
+     *
+     * @param datetime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = $createdAt;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return datetime
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set updated_at
+     *
+     * @param datetime $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updated_at = $updatedAt;
+    }
+
+    /**
+     * Get updated_at
+     *
+     * @return datetime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     */
+    public function prePersist()
+    {
+      $this->setCreatedAt(new \DateTime());
+      $this->setUpdatedAt(new \DateTime());
+    }
+
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function preUpdate()
+    {
+      $this->setUpdatedAt(new \DateTime());
+    }
+}
