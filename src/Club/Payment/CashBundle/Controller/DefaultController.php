1327ab77942038c8328e31d96d83afd166908ae5
Michael Holm <hollo@hollo.dk>
2012-03-12 16:52:41 +0100

    Added payment listeners.

--- src/Club/Payment/CashBundle/Controller/DefaultController.php
+++ src/Club/Payment/CashBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/payment/cash")
      * @Template()
      */
     public function indexAction($name)
--- src/Club/Payment/CashBundle/Listener/Cash.php
+++ src/Club/Payment/CashBundle/Listener/Cash.php
@@ -23,6 +23,7 @@ class Cash
     if (!$method) {
       $method = new \Club\ShopBundle\Entity\PaymentMethod();
       $method->setPaymentMethodName($name);
+      $method->setService($service);
       $method->setPage(<<<EOF
 <h2>Thank you</h2>
 <p>Your order has been successful completed.</p><p>We will complete your order as soon as we receive the payment.</p>
