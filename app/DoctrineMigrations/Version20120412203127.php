200af9843aad702f595b5bfe0d56b70ee833c86e
Michael Holm <hollo@hollo.dk>
2012-04-12 20:48:57 +0200

    Changed migration versions, in order to be able to smooth migrate booking plan.

--- app/DoctrineMigrations/Version20120412203127.php
+++ app/DoctrineMigrations/Version20120412203127.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
 /**
  * Auto-generated Migration: Please modify to your need!
  */
-class Version20120412204044 extends AbstractMigration
+class Version20120412203127 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
