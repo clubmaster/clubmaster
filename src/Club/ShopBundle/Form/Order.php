41b12834e1a21305dd778c5df0a57255c354b5a4
Michael Holm <hollo@hollo.dk>
2012-04-22 13:45:58 +0200

    Added sorting on order status.

--- src/Club/ShopBundle/Form/Order.php
+++ src/Club/ShopBundle/Form/Order.php
@@ -4,12 +4,19 @@ namespace Club\ShopBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Doctrine\ORM\EntityRepository;
 
 class Order extends AbstractType
 {
   public function buildForm(FormBuilder $builder, array $options)
   {
-    $builder->add('order_status');
+    $builder->add('order_status', 'entity', array(
+      'class' => 'Club\ShopBundle\Entity\OrderStatus',
+      'query_builder' => function(EntityRepository $er) {
+        return $er->createQueryBuilder('os')
+          ->orderBy('os.priority', 'ASC');
+      }
+    ));
     $builder->add('note');
   }
 
