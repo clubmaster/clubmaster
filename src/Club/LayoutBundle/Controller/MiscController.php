98e16c65095baa8d48b0e82739a32ccc3be23d1b
Michael Holm <hollo@hollo.dk>
2012-04-27 19:39:35 +0200

    Fixed logo url.

--- src/Club/LayoutBundle/Controller/MiscController.php
+++ src/Club/LayoutBundle/Controller/MiscController.php
@@ -8,9 +8,21 @@ class MiscController extends Controller
 {
   public function logoAction()
   {
+    $logo_url = $this->container->getParameter('club_layout.logo_url');
+    try {
+      $r = $this->get('router')->match($logo_url);
+      $url = $this->get('router')->generate($r['_route']);
+    } catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $e) {
+      if (preg_match("/^http:\/\//", $logo_url)) {
+        $url = $logo_url;
+      } else {
+        $url = 'http://'.$logo_url;
+      }
+    }
+
     return $this->render('ClubLayoutBundle:Misc:logo.html.twig', array(
       'logo_path' => $this->container->getParameter('club_layout.logo_path'),
-      'logo_url' => $this->container->getParameter('club_layout.logo_url'),
+      'logo_url' => $url,
       'logo_title' => $this->container->getParameter('club_layout.logo_title'),
     ));
   }
