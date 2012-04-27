d052b16f3dda6e9c135bdf975382215c318448d6
Michael Holm <hollo@hollo.dk>
2012-04-27 10:24:23 +0200

    Added layout logo configuration

--- src/Club/LayoutBundle/DependencyInjection/ClubLayoutExtension.php
+++ src/Club/LayoutBundle/DependencyInjection/ClubLayoutExtension.php
@@ -11,7 +11,12 @@ class ClubLayoutExtension extends Extension
 {
   public function load(array $configs, ContainerBuilder $container)
   {
+    $configuration = new Configuration();
+    $config = $this->processConfiguration($configuration, $configs);
+
     $loader = new YamlFileLoader($container,new FileLocator(__DIR__.'/../Resources/config'));
     $loader->load('twig.yml');
+
+    $container->setParameter('club_layout.logo', $config['logo']);
   }
 }
