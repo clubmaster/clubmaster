c123abed80afdb1fe0864c10accbb220d006db88
Michael Holm <hollo@hollo.dk>
2012-04-29 16:52:21 +0200

    Clear cache during install.

--- src/Club/ConfiguratorBundle/Controller/ConfiguratorController.php
+++ src/Club/ConfiguratorBundle/Controller/ConfiguratorController.php
@@ -85,6 +85,7 @@ class ConfiguratorController extends ContainerAware
 
         $welcomeUrl = $this->container->get('router')->generate('club_installer_installer_index');
 
+        $this->container->get('club_user.application')->clearCache();
         return $this->container->get('templating')->renderResponse('ClubConfiguratorBundle::Configurator/final.html.twig', array(
             'welcome_url' => $welcomeUrl,
             'parameters'  => $configurator->render(),
