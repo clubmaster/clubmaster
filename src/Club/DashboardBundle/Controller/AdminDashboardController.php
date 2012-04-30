29ea1046abea36ef6bb02c1c7b10514e7a15f1b0
Michael Holm <hollo@hollo.dk>
2012-04-30 14:21:53 +0200

    Moved dashboard to annotation routing.

--- src/Club/DashboardBundle/Controller/AdminDashboardController.php
+++ src/Club/DashboardBundle/Controller/AdminDashboardController.php
@@ -6,7 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-
+/**
+ * @Route("/admin")
+ */
 class AdminDashboardController extends Controller
 {
   /**
