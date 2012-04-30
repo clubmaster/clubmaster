4fad4feffe7d4740c7cbcb2243a442eaca0d1ccc
Michael Holm <hollo@hollo.dk>
2012-04-30 14:18:36 +0200

    Moved teams to annotation routing.

--- src/Club/TeamBundle/Controller/AdminTeamCategoryController.php
+++ src/Club/TeamBundle/Controller/AdminTeamCategoryController.php
@@ -7,6 +7,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
+/**
+ * @Route("/admin/team/category")
+ */
 class AdminTeamCategoryController extends Controller
 {
   /**
