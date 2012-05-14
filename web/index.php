c5de4040b81d7a853c29469b761ec1d5653a69b0
Michael Holm <hollo@hollo.dk>
2012-05-14 10:17:09 +0200

    Upgraded some packages to sf2.1

--- web/index.php
+++ web/index.php
@@ -2,12 +2,14 @@
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/bootstrap_cache.php.cache';
 //require_once __DIR__.'/../app/AppCache.php';
 
 use Symfony\Component\HttpFoundation\Request;
 
-//$kernel = new AppCache(new AppKernel('prod', false));
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-$kernel->handle(Request::createFromGlobals())->send();
+//$kernel = new AppCache($kernel);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
