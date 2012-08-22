b89baf01a8d69347036f94a9e3595ce3e319ca24
Michael Holm <hollo@hollo.dk>
2012-08-22 17:43:03 +0200

    Modified stuff to work with symfony 2.1.

--- web/index.php
+++ web/index.php
@@ -1,10 +1,19 @@
 <?php
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+// Use APC for autoloading to improve performance
+// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
+/*
+$loader = new ApcClassLoader('sf2', $loader);
+$loader->register(true);
+*/
+
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
-
-use Symfony\Component\HttpFoundation\Request;
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
