d2ddfa6484ef89674409eba43fd3ef01dd4c7bac
Michael Holm <hollo@hollo.dk>
2012-09-17 21:41:21 +0200

    Added best_of_set form type.

---
+++ src/Club/LayoutBundle/Form/BestOfSetType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Club\LayoutBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class BestOfSetType extends AbstractType
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'choices' => array(
+                '1' => 1,
+                '3' => 3,
+                '5' => 5,
+            )
+        ));
+    }
+
+    public function getParent()
+    {
+        return 'choice';
+    }
+
+    public function getName()
+    {
+        return 'best_of_set';
+    }
+}
