fd1d211247fa68e878a89bb9971f4e33fbf81dbb
Michael Holm <hollo@hollo.dk>
2012-09-27 17:41:27 +0200

    Cannot delete or pay already payd orders.

--- src/Club/FacebookBundle/DependencyInjection/ClubFacebookExtension.php
+++ src/Club/FacebookBundle/DependencyInjection/ClubFacebookExtension.php
@@ -23,7 +23,7 @@ class ClubFacebookExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        #$loader->load('services.yml');
 
         $container->setParameter('club_facebook.enabled', $config['enabled']);
     }
