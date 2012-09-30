24e66e87a23e73d907caf88d4c84f695b2a4bf60
Michael Holm <hollo@hollo.dk>
2012-10-01 01:15:25 +0200

    Fixed newsticker.

--- src/Club/NewsBundle/Form/Ticker.php
+++ src/Club/NewsBundle/Form/Ticker.php
@@ -3,24 +3,25 @@
 namespace Club\NewsBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class Ticker extends AbstractType
 {
-  public function buildForm(FormBuilder $builder, array $options)
-  {
-    $builder->add('message');
-  }
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('message');
+    }
 
-  public function getDefaultOptions(array $options)
-  {
-    return array(
-      'data_class' => 'Club\NewsBundle\Entity\Ticker'
-    );
-  }
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Club\NewsBundle\Entity\Ticker'
+        ));
+    }
 
-  public function getName()
-  {
-    return 'ticker';
-  }
+    public function getName()
+    {
+        return 'ticker';
+    }
 }
