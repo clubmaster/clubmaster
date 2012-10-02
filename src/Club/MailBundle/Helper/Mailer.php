1399b226531ea9f40113af28871d1ba0bf72c5da
Michael Holm <hollo@hollo.dk>
2012-10-02 23:10:30 +0200

    Made use of decorator plugin.

--- src/Club/MailBundle/Helper/Mailer.php
+++ src/Club/MailBundle/Helper/Mailer.php
@@ -4,79 +4,94 @@ namespace Club\MailBundle\Helper;
 
 class Mailer
 {
-  protected $em;
-  protected $mailer;
-  protected $container;
-  protected $message;
+    protected $em;
+    protected $mailer;
+    protected $container;
+    protected $message;
 
-  public function __construct($container)
-  {
-    $this->container = $container;
-    $this->em = $container->get('doctrine.orm.entity_manager');
-    $this->mailer = $container->get('mailer');
+    public function __construct($container)
+    {
+        $this->container = $container;
+        $this->em = $container->get('doctrine.orm.entity_manager');
+        $this->mailer = $container->get('mailer');
 
-    $this->message = \Swift_Message::newInstance();
-  }
-
-  public function setFrom($sender_addr = null, $sender_name = null)
-  {
-    if ($sender_addr == null)
-      $sender_addr = $this->em->getRepository('ClubUserBundle:LocationConfig')->getObjectByKey('email_sender_address');
-
-    if ($sender_name == null)
-      $sender_name = $this->em->getRepository('ClubUserBundle:LocationConfig')->getObjectByKey('email_sender_name');
-
-    $sender_addr = ($sender_addr) ? $sender_addr : $this->container->getParameter('club_mail.default_sender_address');
-    $sender_name = ($sender_name) ? $sender_name : $this->container->getParameter('club_mail.default_sender_name');
-
-    $this->message->setFrom(array(
-       $sender_addr => $sender_name
-    ));
-
-    return $this;
-  }
-
-  public function setSubject($subject)
-  {
-    $this->message->setSubject($subject);
-
-    return $this;
-  }
-
-  public function setTo($to)
-  {
-    try {
-      $this->message->setTo($to);
-    } catch (\Exception $e) {
-      // we tried to set an invalid email address
+        $this->message = \Swift_Message::newInstance();
     }
 
-    return $this;
-  }
+    public function setDecorator($recipients)
+    {
+        $replacements = array();
+        foreach ($recipients as $user) {
+            $replacements[$user->getEmail()] = array(
+                '{name}' => $user->getName()
+            );
+        }
 
-  public function setBody($body, $type=null)
-  {
-    $this->message->setBody($body, $type);
+        $decorator = new \Swift_Plugins_DecoratorPlugin($replacements);
+        $this->mailer->registerPlugin($decorator);
 
-    return $this;
-  }
-
-  public function attach($attachment)
-  {
-    $this->message->attach(
-      \Swift_Attachment::fromPath($attachment->getAbsolutePath())
-      ->setFilename($attachment->getFileName())
-    );
-
-    return $this;
-  }
-
-  public function send()
-  {
-    try {
-      $this->mailer->send($this->message);
-    } catch (\Exception $e) {
-      // some error has occur, the email is not valid
+        return $this;
     }
-  }
+
+    public function setFrom($sender_addr = null, $sender_name = null)
+    {
+        if ($sender_addr == null)
+            $sender_addr = $this->em->getRepository('ClubUserBundle:LocationConfig')->getObjectByKey('email_sender_address');
+
+        if ($sender_name == null)
+            $sender_name = $this->em->getRepository('ClubUserBundle:LocationConfig')->getObjectByKey('email_sender_name');
+
+        $sender_addr = ($sender_addr) ? $sender_addr : $this->container->getParameter('club_mail.default_sender_address');
+        $sender_name = ($sender_name) ? $sender_name : $this->container->getParameter('club_mail.default_sender_name');
+
+        $this->message->setFrom(array(
+            $sender_addr => $sender_name
+        ));
+
+        return $this;
+    }
+
+    public function setSubject($subject)
+    {
+        $this->message->setSubject($subject);
+
+        return $this;
+    }
+
+    public function setTo($to)
+    {
+        try {
+            $this->message->addTo($to);
+        } catch (\Exception $e) {
+            // we tried to set an invalid email address
+        }
+
+        return $this;
+    }
+
+    public function setBody($body, $type=null)
+    {
+        $this->message->setBody($body, $type);
+
+        return $this;
+    }
+
+    public function attach($attachment)
+    {
+        $this->message->attach(
+            \Swift_Attachment::fromPath($attachment->getAbsolutePath())
+            ->setFilename($attachment->getFileName())
+        );
+
+        return $this;
+    }
+
+    public function send()
+    {
+        try {
+            $this->mailer->send($this->message);
+        } catch (\Exception $e) {
+            // some error has occur, the email is not valid
+        }
+    }
 }
--- src/Club/MailBundle/Listener/BookingListener.php
+++ src/Club/MailBundle/Listener/BookingListener.php
@@ -4,94 +4,98 @@ namespace Club\MailBundle\Listener;
 
 class BookingListener
 {
-  protected $container;
-  protected $em;
-  protected $templating;
-  protected $router;
-  protected $translator;
-  protected $clubmaster_mailer;
+    protected $container;
+    protected $em;
+    protected $templating;
+    protected $router;
+    protected $translator;
+    protected $clubmaster_mailer;
 
-  public function __construct($container)
-  {
-    $this->container = $container;
-    $this->em = $container->get('doctrine.orm.entity_manager');
-    $this->templating = $container->get('templating');
-    $this->router = $container->get('router');
-    $this->translator = $container->get('translator');
-    $this->clubmaster_mailer = $container->get('clubmaster_mailer');
-  }
-
-  public function onBookingConfirm(\Club\BookingBundle\Event\FilterBookingEvent $event)
-  {
-    $booking = $event->getBooking();
-    $recipients = $this->getRecipients($booking);
-
-    $this->clubmaster_mailer
-      ->setSubject($this->translator->trans('Booking confirm'))
-      ->setFrom();
-
-    foreach ($recipients as $user) {
-      $this->clubmaster_mailer
-        ->setTo($user->getProfile()->getProfileEmail()->getEmailAddress())
-        ->setBody($this->templating->render('ClubMailBundle:Template:booking_confirm.html.twig', array(
-          'user' => $user,
-          'booking' => $booking,
-        )))
-        ->send()
-        ;
-    }
-  }
-
-  public function onBookingCancel(\Club\BookingBundle\Event\FilterBookingEvent $event)
-  {
-    $booking = $event->getBooking();
-    $recipients = $this->getRecipients($booking);
-
-    $this->clubmaster_mailer
-      ->setSubject($this->translator->trans('Booking cancel'))
-      ->setFrom();
-
-    foreach ($recipients as $user) {
-      $this->clubmaster_mailer
-        ->setTo($user->getProfile()->getProfileEmail()->getEmailAddress())
-        ->setBody($this->templating->render('ClubMailBundle:Template:booking_cancel.html.twig',array(
-          'user' => $user,
-          'booking' => $booking,
-        )))
-        ->send();
-    }
-  }
-
-  protected function getRecipients($booking)
-  {
-    $recipients = array();
-
-    if ($booking->getUser()->getProfile()->getProfileEmail()) {
-      if ($this->receiveMail($booking->getUser())) {
-        $recipients[] = $booking->getUser();
-      }
+    public function __construct($container)
+    {
+        $this->container = $container;
+        $this->em = $container->get('doctrine.orm.entity_manager');
+        $this->templating = $container->get('templating');
+        $this->router = $container->get('router');
+        $this->translator = $container->get('translator');
+        $this->clubmaster_mailer = $container->get('clubmaster_mailer');
     }
 
-    foreach ($booking->getUsers() as $user) {
-      if ($this->receiveMail($user)) {
-        if ($user->getProfile()->getProfileEmail()) {
-          $recipients[] = $user;
+    public function onBookingConfirm(\Club\BookingBundle\Event\FilterBookingEvent $event)
+    {
+        $booking = $event->getBooking();
+        $recipients = $this->getRecipients($booking);
+
+        $this->clubmaster_mailer
+            ->setSubject($this->translator->trans('Booking confirm'))
+            ->setFrom()
+            ->setBody($this->templating->render('ClubMailBundle:Template:booking_confirm.html.twig', array(
+                'user' => $booking->getUser(),
+                'booking' => $booking,
+            )))
+            ->setDecorator($recipients)
+            ;
+
+        foreach ($recipients as $user) {
+            $this->clubmaster_mailer
+                ->setTo($user->getEmail())
+                ->send()
+                ;
         }
-      }
     }
 
-    return $recipients;
-  }
+    public function onBookingCancel(\Club\BookingBundle\Event\FilterBookingEvent $event)
+    {
+        $booking = $event->getBooking();
+        $recipients = $this->getRecipients($booking);
 
-  protected function receiveMail(\Club\UserBundle\Entity\User $user)
-  {
-    $s = $this->em->getRepository('ClubUserBundle:UserSetting')->findOneBy(array(
-      'user' => $user->getId(),
-      'attribute' => 'receive_email_on_booking'
-    ));
-    if ($s && !$s->getValue()) return false;
-    if (!$s && !$this->container->getParameter('club_mail.mail_on_booking')) return false;
+        $this->clubmaster_mailer
+            ->setSubject($this->translator->trans('Booking cancel'))
+            ->setFrom()
+            ->setBody($this->templating->render('ClubMailBundle:Template:booking_cancel.html.twig',array(
+                'user' => $user,
+                'booking' => $booking,
+            )))
+            ->setDecorator($recipients);
 
-    return true;
-  }
+
+        foreach ($recipients as $user) {
+            $this->clubmaster_mailer
+                ->setTo($user->getProfile()->getProfileEmail()->getEmailAddress())
+                ->send();
+        }
+    }
+
+    protected function getRecipients($booking)
+    {
+        $recipients = array();
+
+        if ($booking->getUser()->getProfile()->getProfileEmail()) {
+            if ($this->receiveMail($booking->getUser())) {
+                $recipients[] = $booking->getUser();
+            }
+        }
+
+        foreach ($booking->getUsers() as $user) {
+            if ($this->receiveMail($user)) {
+                if ($user->getProfile()->getProfileEmail()) {
+                    $recipients[] = $user;
+                }
+            }
+        }
+
+        return $recipients;
+    }
+
+    protected function receiveMail(\Club\UserBundle\Entity\User $user)
+    {
+        $s = $this->em->getRepository('ClubUserBundle:UserSetting')->findOneBy(array(
+            'user' => $user->getId(),
+            'attribute' => 'receive_email_on_booking'
+        ));
+        if ($s && !$s->getValue()) return false;
+        if (!$s && !$this->container->getParameter('club_mail.mail_on_booking')) return false;
+
+        return true;
+    }
 }
