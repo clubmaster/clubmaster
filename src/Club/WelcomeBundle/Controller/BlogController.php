e67de1de2db9f4df4fa85573d30bdf4718ab5413
Michael Holm <hollo@hollo.dk>
2012-10-04 18:29:04 +0200

    Added facebook like and google plus like to blog posts.

--- src/Club/WelcomeBundle/Controller/BlogController.php
+++ src/Club/WelcomeBundle/Controller/BlogController.php
@@ -24,6 +24,9 @@ class BlogController extends Controller
 
     return array(
       'blog' => $blog,
+      'google_plus' => $this->container->getParameter('club_welcome.google_plus'),
+      'facebook_like' => $this->container->getParameter('club_welcome.facebook_like'),
+      'facebook_api' => $this->container->getParameter('club_welcome.facebook_api')
     );
   }
 
--- src/Club/WelcomeBundle/DependencyInjection/Configuration.php
+++ src/Club/WelcomeBundle/DependencyInjection/Configuration.php
@@ -24,6 +24,13 @@ class Configuration implements ConfigurationInterface
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
 
+        $rootNode
+            ->children()
+            ->scalarNode('google_plus')->isRequired()->end()
+            ->scalarNode('facebook_like')->isRequired()->end()
+            ->scalarNode('facebook_api')->isRequired()->end()
+            ->end();
+
         return $treeBuilder;
     }
 }
