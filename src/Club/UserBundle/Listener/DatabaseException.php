ba42d8f874fe66c25acde1171e5c717f07b4a650
Michael Holm <hollo@hollo.dk>
2012-10-09 11:10:44 +0200

    Updated TODO

--- src/Club/UserBundle/Listener/DatabaseException.php
+++ src/Club/UserBundle/Listener/DatabaseException.php
@@ -4,6 +4,7 @@ namespace Club\UserBundle\Listener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DatabaseException
 {
@@ -13,7 +14,25 @@ class DatabaseException
         $class = get_class($exception);
         if (preg_match('/(Doctrine\\\\DBAL\\\\DBALException|PDOException)/', $class)) {
             if (preg_match("/(Unknown database|Base table or view not found)/", $exception->getMessage())) {
-                $response = new Response('Please visit the setup.php page.');
+
+                $path = 'web/setup.php';
+                $not_found = true;
+
+                for ($i = 0; $not_found; $i++) {
+                    if (is_file($path)) {
+                        $response = new RedirectResponse('../'.$path);
+                        $not_found = false;
+                    } else {
+                        $path = '../'.$path;
+                    }
+
+                    if ($i > 5) break;
+                }
+
+                if (!isset($response)) {
+                    $response = new Response('Please visit the setup.php page.');
+                }
+
                 $event->setResponse($response);
             }
         }
