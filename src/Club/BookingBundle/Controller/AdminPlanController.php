4648a41568092fb048566391ddfe6a4f5f709ba0
Michael Holm <hollo@hollo.dk>
2012-10-11 16:40:44 +0200

    Added code to proof of concept to move work.

--- src/Club/BookingBundle/Controller/AdminPlanController.php
+++ src/Club/BookingBundle/Controller/AdminPlanController.php
@@ -17,6 +17,35 @@ class AdminPlanController extends Controller
      */
     public function indexAction()
     {
+        $data = <<<EOF
+BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+SUMMARY:Curiosity landing
+DTSTART:20120806T051439Z
+LOCATION:Mars
+END:VEVENT
+BEGIN:VEVENT
+UID:1102c450-e0d7-11e1-9b23-0800200c9a66
+DTSTART:20120109T140000Z
+RRULE:FREQ=MONTHLY;BYDAY=MO;BYSETPOS=2
+END:VEVENT
+END:VCALENDAR
+EOF;
+
+        $start = new \DateTime('2012-01-01');
+        $end = new \DateTime('2012-12-31');
+
+        $calendar = \Sabre\VObject\Reader::read($data);
+        $calendar->expand($start, $end);
+
+        /**
+        foreach ($calendar->VEVENT as $event) {
+            var_dump($event->DTSTART->getDateTime());
+        }
+        die();
+         */
+
         $em = $this->getDoctrine()->getEntityManager();
 
         $plans = $em->getRepository('ClubBookingBundle:Plan')->findBy(
