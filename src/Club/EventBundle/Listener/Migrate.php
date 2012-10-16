3ee4fe4053da40daa36fb8b23e37d1dbc17ccec8
Michael Holm <hollo@hollo.dk>
2012-10-16 17:52:03 +0200

    Added migrate events.

---
+++ src/Club/EventBundle/Listener/Migrate.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Club\EventBundle\Listener;
+
+class Migrate
+{
+    private $container;
+    private $em;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+        $this->em = $container->get('doctrine.orm.entity_manager');
+    }
+
+    public function onVersionMigrate(\Club\InstallerBundle\Event\FilterVersionEvent $event)
+    {
+        if ($event->getVersion()->getVersion() != '20121016161300') {
+            // fit to this version only
+            return;
+        }
+
+        $events = $this->em->getRepository('ClubEventBundle:Event')->findAll();
+
+        foreach ($events as $event) {
+            $date = clone $event->getStartDate();
+            $date->modify('-1 week');
+
+            $event->setLastSubscribe($date);
+            $this->em->persist($event);
+
+        }
+    }
+}
