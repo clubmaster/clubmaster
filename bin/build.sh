0cc3d3a790c08e5cd8ce69027649761d3f835b26
Michael Holm <hollo@hollo.dk>
2012-10-20 11:09:24 +0200

    Updated README Improved build script.

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 VERSION=`cat src/Club/UserBundle/Helper/Version.php  | grep "protected $version" | cut -d"'" -f2 | cut -d"-" -f1`
 BUILD_PATH=/tmp/clubmaster_${VERSION}
@@ -7,28 +7,40 @@ if [ -d "${BUILD_PATH}" ]; then
   rm -rf ${BUILD_PATH}
 fi
 
-echo "Testing remake_schema"
-./bin/remake_schema.sh
-if [ $? -ne 0 ]; then
-  exit
+echo -n "Run unit tests (YES|no)"
+read a
+
+if [ "$a" == "no" ]; then
+    unittest=0
+else
+    unittest=1
 fi
 
-echo "Testing all other"
-phpunit -c app/phpunit.xml.dist
-if [ $? -ne 0 ]; then
-  exit
+if [ ${unittest} -eq 1 ]; then
+    echo "Testing remake_schema"
+    ./bin/remake_schema.sh
+    if [ $? -ne 0 ]; then
+        exit
+    fi
+
+    echo "Testing all other"
+    phpunit -c app/phpunit.xml.dist
+    if [ $? -ne 0 ]; then
+        exit
+    fi
+
+    echo "Testing API"
+    phpunit -c app/apiunit.xml
+    if [ $? -ne 0 ]; then
+        exit
+    fi
+    echo "Tests was good"
 fi
 
-echo "Testing API"
-phpunit -c app/apiunit.xml
-if [ $? -ne 0 ]; then
-  exit
-fi
+echo "Starting to build package."
+mkdir ${BUILD_PATH}
 
 php app/console assetic:dump
-
-echo "Everything OK, continue to build package."
-mkdir ${BUILD_PATH}
 
 TRUNK_PATH=`pwd`
 
@@ -36,15 +48,13 @@ cp -r ${TRUNK_PATH}/* ${BUILD_PATH}
 
 sed -i 's/\-dev//' ${BUILD_PATH}/src/Club/UserBundle/Helper/Version.php
 rm -rf ${BUILD_PATH}/bin
-rm -rf ${BUILD_PATH}/deps
 rm -rf ${BUILD_PATH}/app/sql
 rm -rf ${BUILD_PATH}/app/cache/*
 rm -rf ${BUILD_PATH}/app/logs/*
-rm -rf ${BUILD_PATH}/deps.lock
+rm -rf ${BUILD_PATH}/composer.*
 rm -rf ${BUILD_PATH}/app/phpunit.xml.dist
 rm -rf ${BUILD_PATH}/web/uploads/*
 
-rm ${SQL_DUMP_FILE}
 find ${BUILD_PATH} -name .git | xargs rm -rf
 find ${BUILD_PATH} -name .gitkeep | xargs rm -rf
 find ${BUILD_PATH} -name .gitignore | xargs rm -rf
