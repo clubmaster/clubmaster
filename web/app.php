6a6eb18c1f2bba7d9f600412670af88dc72bd2c6
Michael Holm <hollo@hollo.dk>
2012-10-20 14:32:23 +0200

    optimized app.php.

--- web/app.php
+++ web/app.php
@@ -10,26 +10,18 @@ if (!is_writeable('../app/cache') && is_file('setup.php')) {
     exit;
 }
 
-$apc = (function_exists('apc_cache_info')) ? true : false;
-
-// Use APC for autoloading to improve performance
-// Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-if ($apc) {
-    $loader = new ApcClassLoader('sf2', $loader);
-    $loader->register(true);
-}
-
 require_once __DIR__.'/../app/AppKernel.php';
+$kernel = new AppKernel('prod', false);
 
+$apc = (function_exists('apc_cache_info')) ? true : false;
 if ($apc) {
     require_once __DIR__.'/../app/AppCache.php';
-}
-
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-
-if ($apc) {
     $kernel = new AppCache($kernel);
+
+    $loader = new ApcClassLoader('sf2', $loader);
+    $loader->register(true);
+} else {
+    $kernel->loadClassCache();
 }
 
 $request = Request::createFromGlobals();
