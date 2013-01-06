df9413cc8462b33f9ab198947f5b3b6cdd9d6da4
Michael Holm <hollo@hollo.dk>
2013-01-06 16:58:14 +0100

    Changed default form theme.

--- src/Club/FormExtraBundle/DependencyInjection/ClubFormExtraExtension.php
+++ src/Club/FormExtraBundle/DependencyInjection/ClubFormExtraExtension.php
@@ -22,7 +22,7 @@ class ClubFormExtraExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new YamlFileLoader($container,new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('twig.yml');
         $loader->load('form.yml');
     }
