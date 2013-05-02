caf56ad828b467df14b5abfc9ab156eabcb3c982
Michael Holm <hollo@hollo.dk>
2013-05-02 23:09:05 +0200

    Added support for paginator on products in shop

--- src/Club/ShopBundle/Entity/ProductRepository.php
+++ src/Club/ShopBundle/Entity/ProductRepository.php
@@ -12,19 +12,22 @@ use Doctrine\ORM\EntityRepository;
  */
 class ProductRepository extends EntityRepository
 {
-    public function getByCategory(\Club\ShopBundle\Entity\Category $category, $limit=10)
+    public function getByCategory(\Club\ShopBundle\Entity\Category $category, $limit=10, $page = 0)
     {
-        return $this->createQueryBuilder('p')
+        $offset = ($page < 1) ? 1 : ($page-1)*$limit;
+
+        $qb = $this->createQueryBuilder('p')
             ->join('p.categories', 'c')
             ->where('c.id = :category')
             ->andWhere('p.active = true')
             ->andWhere('p.status = :status')
             ->orderBy('p.priority', 'DESC')
+            ->setFirstResult($offset)
             ->setMaxResults($limit)
             ->setParameter('category', $category)
-            ->setParameter('status', \Club\ShopBundle\Entity\Product::ACTIVE)
-            ->getQuery()
-            ->getResult();
+            ->setParameter('status', \Club\ShopBundle\Entity\Product::ACTIVE);
+
+        return new \Doctrine\ORM\Tools\Pagination\Paginator($qb, false);
     }
 
     public function getUsersByProduct(\Club\ShopBundle\Entity\Product $product)
@@ -61,4 +64,20 @@ class ProductRepository extends EntityRepository
             ->getQuery()
             ->getResult();
     }
+
+    public function getPaginator($limit = 10, $page = 0)
+    {
+        $offset = ($page < 1) ? 1 : ($page-1)*$limit;
+
+        $qb = $this->createQueryBuilder('p')
+            ->where('p.status = :status')
+            ->andWhere('p.active = true')
+            ->orderBy('p.priority', 'DESC')
+            ->setFirstResult($offset)
+            ->setMaxResults($limit)
+            ->setParameter('status', \Club\ShopBundle\Entity\Product::ACTIVE);
+
+        return new \Doctrine\ORM\Tools\Pagination\Paginator($qb, false);
+    }
+
 }
