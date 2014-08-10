08058cfe4b5ab9aec7f6d71151c21fa3bff41312
Michael Holm <hollo@hollo.dk>
2014-08-10 20:43:30 +0200

    Added some div panes.

--- src/Club/DashboardBundle/Controller/DashboardController.php
+++ src/Club/DashboardBundle/Controller/DashboardController.php
@@ -8,28 +8,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DashboardController extends Controller
 {
-  /**
-   * @Template()
-   * @Route("/user/dashboard")
-   */
-  public function indexAction()
-  {
-    $event = new \Club\UserBundle\Event\FilterOutputEvent($this->getUser());
-    $this->get('event_dispatcher')->dispatch(\Club\DashboardBundle\Event\Events::onDashboardView, $event);
+    /**
+     * @Template()
+     * @Route("/user/dashboard")
+     */
+    public function indexAction()
+    {
+        $event = new \Club\UserBundle\Event\FilterOutputEvent($this->getUser());
+        $this->get('event_dispatcher')->dispatch(\Club\DashboardBundle\Event\Events::onDashboardView, $event);
 
-    $event2 = new \Club\UserBundle\Event\FilterActivityEvent($this->getUser());
-    $this->get('event_dispatcher')->dispatch(\Club\DashboardBundle\Event\Events::onDashboardComing, $event2);
+        $event2 = new \Club\UserBundle\Event\FilterActivityEvent($this->getUser());
+        $this->get('event_dispatcher')->dispatch(\Club\DashboardBundle\Event\Events::onDashboardComing, $event2);
 
-    $activities = $event2->getActivities();
-    ksort($activities);
+        $activities = $event2->getActivities();
+        ksort($activities);
 
-    $event3 = new \Club\UserBundle\Event\FilterActivityEvent($this->getUser());
-    $this->get('event_dispatcher')->dispatch(\Club\DashboardBundle\Event\Events::onDashboardRecent, $event3);
+        $event3 = new \Club\UserBundle\Event\FilterActivityEvent($this->getUser());
+        $this->get('event_dispatcher')->dispatch(\Club\DashboardBundle\Event\Events::onDashboardRecent, $event3);
 
-    return array(
-      'output' => $event->getOutput(),
-      'activities' => $activities,
-      'recent' => $event3->getActivities()
-    );
-  }
+        return array(
+            'output' => $event->getOutput(),
+            'activities' => $activities,
+            'recent' => $event3->getActivities()
+        );
+    }
 }
