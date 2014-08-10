51f2412122f0f9f391744115d4214b94380c5ade
Michael Holm <hollo@hollo.dk>
2014-08-10 20:26:31 +0200

    Fixed feedback form.

--- src/Club/FeedbackBundle/Controller/FeedbackController.php
+++ src/Club/FeedbackBundle/Controller/FeedbackController.php
@@ -11,93 +11,111 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  */
 class FeedbackController extends Controller
 {
-  /**
-   * @Route("")
-   * @Template()
-   */
-  public function indexAction()
-  {
-    $type = array(
-      'idea' => $this->get('translator')->trans('Idea'),
-      'bug' => $this->get('translator')->trans('Bug'),
-      'other' => $this->get('translator')->trans('Other')
-    );
+    /**
+     * @Route("")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $type = array(
+            'idea' => $this->get('translator')->trans('Idea'),
+            'bug' => $this->get('translator')->trans('Bug'),
+            'other' => $this->get('translator')->trans('Other')
+        );
 
-    $feedback = new \Club\FeedbackBundle\Model\Feedback();
-    if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-      $user = $this->getUser();
-      $feedback->name = $user->getProfile()->getName();
+        $feedback = new \Club\FeedbackBundle\Model\Feedback();
+        if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $user = $this->getUser();
+            $feedback->name = $user->getProfile()->getName();
 
-      if ($user->getProfile()->getProfileEmail())
-        $feedback->email = $user->getProfile()->getProfileEmail()->getEmailAddress();
-    }
-    $form = $this->createFormBuilder($feedback)
-      ->add('name','text',array(
-        'required' => false
-      ))
-      ->add('email','email',array(
-        'required' => false
-      ))
-      ->add('type','choice',array(
-        'choices' => $type
-      ))
-      ->add('subject','text',array(
-        'required' => true
-      ))
-      ->add('message','textarea',array(
-          'required' => true,
-          'attr' => array(
-              'class' => 'big'
-          )
-      ))
-      ->getForm();
+            if ($user->getProfile()->getProfileEmail())
+                $feedback->email = $user->getProfile()->getProfileEmail()->getEmailAddress();
+        }
 
-    if ($this->getRequest()->getMethod() == 'POST') {
-      $form->bind($this->getRequest());
+        $attr = array(
+            'class' => 'form-control'
+        );
+        $label_attr = array(
+            'class' => 'col-sm-2'
+        );
 
-      if ($form->isValid()) {
-        $this->sendData($feedback);
+        $form = $this->createFormBuilder($feedback)
+            ->add('name','text',array(
+                'attr' => $attr,
+                'label_attr' => $label_attr,
+                'required' => false
+            ))
+            ->add('email','email',array(
+                'attr' => $attr,
+                'label_attr' => $label_attr,
+                'required' => false
+            ))
+            ->add('type','choice',array(
+                'attr' => $attr,
+                'label_attr' => $label_attr,
+                'choices' => $type
+            ))
+            ->add('subject','text',array(
+                'attr' => $attr,
+                'label_attr' => $label_attr,
+                'required' => true
+            ))
+            ->add('message','textarea',array(
+                'attr' => array(
+                    'class' => $attr['class'],
+                    'rows' => 10
+                ),
+                'label_attr' => $label_attr,
+                'required' => true,
+            ))
+            ->getForm();
 
-        return $this->redirect($this->generateUrl('club_feedback_feedback_index'));
-      }
-    }
+        if ($this->getRequest()->getMethod() == 'POST') {
+            $form->bind($this->getRequest());
 
-    return array(
-      'form' => $form->createView()
-    );
-  }
+            if ($form->isValid()) {
+                $this->sendData($feedback);
 
-  private function sendData(\Club\FeedbackBundle\Model\Feedback $feedback)
-  {
-    $host = 'loopback.clubmaster.dk';
-    $fp = @fsockopen($host,80);
+                return $this->redirect($this->generateUrl('club_feedback_feedback_index'));
+            }
+        }
 
-    $feedback->host = $this->generateUrl('homepage', array(), true);
-    $str = '';
-    foreach ($feedback as $key=>$value) {
-      $str .= $key.'='.$value.'&';
-    }
-
-    if ($fp) {
-      fputs($fp, "POST /feedback HTTP/1.1\r\n");
-      fputs($fp, "HOST: loopback.clubmaster.dk\r\n");
-      fputs($fp, "User-Agent: ClubMasterTool 1.0\r\n");
-      fputs($fp, "Content-length: ".strlen($str)."\r\n");
-      fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n\r\n");
-
-      fputs($fp, $str."\r\n\r\n");
-
-      $res = '';
-      while (!feof($fp)) $res .= fgets($fp,4096);
-      fclose($fp);
-
-      if (strpos($res,'200 OK')) {
-        $this->get('session')->getFlashBag()->add('notice',$this->get('translator')->trans('Your message has been sent.'));
-
-        return;
-      }
+        return array(
+            'form' => $form->createView()
+        );
     }
 
-    $this->get('session')->getFlashBag()->add('error',$this->get('translator')->trans('There was a problem delivering your message.'));
-  }
+    private function sendData(\Club\FeedbackBundle\Model\Feedback $feedback)
+    {
+        $host = 'loopback.clubmaster.dk';
+        $fp = @fsockopen($host,80);
+
+        $feedback->host = $this->generateUrl('homepage', array(), true);
+        $str = '';
+        foreach ($feedback as $key=>$value) {
+            $str .= $key.'='.$value.'&';
+        }
+
+        if ($fp) {
+            fputs($fp, "POST /feedback HTTP/1.1\r\n");
+            fputs($fp, "HOST: loopback.clubmaster.dk\r\n");
+            fputs($fp, "User-Agent: ClubMasterTool 1.0\r\n");
+            fputs($fp, "Content-length: ".strlen($str)."\r\n");
+            fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n\r\n");
+
+            fputs($fp, $str."\r\n\r\n");
+
+            $res = '';
+            while (!feof($fp)) $res .= fgets($fp,4096);
+            fclose($fp);
+
+            if (strpos($res,'200 OK')) {
+                $this->get('session')->getFlashBag()->add('notice',$this->get('translator')->trans('Your message has been sent.'));
+
+                return;
+            }
+        }
+
+        $this->get('session')->getFlashBag()->add('error',$this->get('translator')->trans('There was a problem delivering your message.'));
+    }
 }
